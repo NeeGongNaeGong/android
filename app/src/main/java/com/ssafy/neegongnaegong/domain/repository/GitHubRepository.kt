b6bb1e8ed7a3b54442bad3e86c8eb95bfdfafe31
김민조 <upsk1@naver.com>
2025-02-22 00:44:04 +0900

    feat: domain layer

---
+++ app/src/main/java/com/ssafy/neegongnaegong/domain/repository/GitHubRepository.kt
@@ -0,0 +1,7 @@
+package com.ssafy.neegongnaegong.domain.repository
+
+import com.ssafy.neegongnaegong.domain.model.GitHubRepo
+
+interface GitHubRepository {
+    suspend fun getUserRepos(username: String): List<GitHubRepo>
+}
