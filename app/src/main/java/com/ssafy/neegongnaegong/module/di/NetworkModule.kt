bf7f5ccd17ed2d3c808c161774048c7f4a78b3e3
김민조 <upsk1@naver.com>
2025-02-22 05:28:44 +0900

    feat: module, di 패키지

---
+++ app/src/main/java/com/ssafy/neegongnaegong/module/di/NetworkModule.kt
@@ -0,0 +1,41 @@
+package com.ssafy.neegongnaegong.module.di
+
+import com.ssafy.neegongnaegong.data.remote.GitHubApi
+import com.ssafy.neegongnaegong.data.repository.GitHubRepositoryImpl
+import com.ssafy.neegongnaegong.domain.repository.GitHubRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    // Server꺼는 local properties에 숨김처리
+    private const val BASE_URL = "https://api.github.com/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGitHubApi(retrofit: Retrofit): GitHubApi {
+        return retrofit.create(GitHubApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGitHubRepository(api: GitHubApi): GitHubRepository {
+        return GitHubRepositoryImpl(api)
+    }
+}
