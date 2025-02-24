c3f8de881257d223a4a6d32c407dd6bdcba03507
jaehan4707 <jahan4707@naver.com>
2025-02-24 14:47:05 +0900

    ui(#3): LightColors, DarkColors 추가, Theme 라이트 테마 ,다크 테마 적용

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/ui/theme/Color.kt
@@ -2,10 +2,32 @@ package com.ssafy.neegongnaegong.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+object LightColors {
+    val Blue = Color(0xFF82ACCF)
+    val MintBlue = Color(0xFFB5E7E5)
+    val Mint = Color(0xFFA5F7CB)
+    val LightGreen = Color(0xFFD2Fc90)
+    val Peach = Color(0xFFFFBBBB)
+    val Black = Color(0xFF222227)
+    val Gray4 = Color(0xFF949494)
+    val Gray3 = Color(0xFFD9D9D9)
+    val Gray2 = Color(0xFFF4F4F4)
+    val Gray1 = Color(0xFFF9F9F9)
+    val White = Color(0xFFFFFFFF)
+    val BackGround = Color(0xFFFAFAFA)
+}
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+object DarkColors {
+    val DarkBlue = Color(0xFF4A708E)
+    val DarkMintBlue = Color(0xFF7FCBCB)
+    val DarkMint = Color(0xFF6FCFA6)
+    val DarkLightGreen = Color(0xFF9BBB5C)
+    val DarkPeach = Color(0xFFCC8888)
+    val DarkBlack = Color(0xFF121212)
+    val DarkGray4 = Color(0xFF666666)
+    val DarkGray3 = Color(0xFF555555)
+    val DarkGray2 = Color(0xFF333333)
+    val DarkGray1 = Color(0xFF222222)
+    val DarkWhite = Color(0xFFEFEFEF)
+    val DarkBackGround = Color(0xFF121212)
+}
