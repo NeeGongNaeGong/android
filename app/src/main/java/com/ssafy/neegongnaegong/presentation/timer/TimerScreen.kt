df932ee5800850ec58483e2f0b8f69e04f746020
김민조 <upsk1@naver.com>
2025-03-04 14:53:45 +0900

    feat: TimerScreen 생성

---
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/timer/TimerScreen.kt
@@ -0,0 +1,18 @@
+package com.ssafy.neegongnaegong.presentation.timer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongTheme
+
+@Composable
+fun TimerScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TimerPreview() {
+    NeeGongNaeGongTheme {
+        TimerScreen()
+    }
+}
