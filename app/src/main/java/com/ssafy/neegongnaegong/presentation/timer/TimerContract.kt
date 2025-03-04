12e3a0c83e24042edc61a890546b6bbad416fad9
김민조 <upsk1@naver.com>
2025-03-04 14:59:43 +0900

    feat: Timer Contract 생성

---
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/timer/TimerContract.kt
@@ -0,0 +1,25 @@
+package com.ssafy.neegongnaegong.presentation.timer
+
+import com.ssafy.neegongnaegong.domain.model.GitHubRepo
+import com.ssafy.neegongnaegong.presentation.base.UiEffect
+import com.ssafy.neegongnaegong.presentation.base.UiEvent
+import com.ssafy.neegongnaegong.presentation.base.UiState
+
+class TimerContract {
+    sealed class Event : UiEvent {
+        data object OnStopClicked : Event()
+    }
+
+    data class State(val timerState: TimerState) : UiState
+
+    // todo retrofit 통신 연결 시 상태 수정
+    sealed class TimerState {
+        data object Idle : TimerState()
+        data object Loading : TimerState()
+        data object Success : TimerState()
+    }
+
+    sealed class Effect : UiEffect {
+
+    }
+}
