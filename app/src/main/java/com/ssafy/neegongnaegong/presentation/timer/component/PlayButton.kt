cec7c0cf10bb67b2b31262a1d4727ba141bdacbf
김민조 <upsk1@naver.com>
2025-03-25 15:32:43 +0900

    ui(#7): 버튼 디자인 통일

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/timer/component/PlayButton.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/timer/component/PlayButton.kt
@@ -1,11 +1,16 @@
 package com.ssafy.neegongnaegong.presentation.timer.component
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -14,16 +19,24 @@ import com.ssafy.neegongnaegong.R
 
 @Composable
 fun PlayButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    val interactionSource = remember { MutableInteractionSource() }
 
-    IconButton(
-        modifier = modifier.size(100.dp),
-        onClick = onClick
+    OutlinedButton(
+        modifier = modifier
+            .size(88.dp),
+        onClick = onClick,
+        shape = CircleShape,
+        border = BorderStroke(4.dp, Color.Black),
+        colors = ButtonDefaults.outlinedButtonColors(
+            contentColor = Color.Black,
+            containerColor = Color.Transparent
+        ),
+        interactionSource = interactionSource
     ) {
         Icon(
             imageVector = Icons.Default.PlayArrow,
             contentDescription = stringResource(R.string.txt_timer_play),
-            tint = Color.Black,
-            modifier = Modifier.size(80.dp)
+            modifier = Modifier.size(60.dp)
         )
     }
 }
