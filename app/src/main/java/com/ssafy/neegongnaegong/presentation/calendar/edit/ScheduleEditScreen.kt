798b8c646514b3df4fb6c22977c550be7e96dd3e
박준식 <akfldkskgorn@gmail.com>
2025-03-28 00:34:23 +0900

    ui(#17): ScheduleEditScreen 구현

---
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/calendar/edit/ScheduleEditScreen.kt
@@ -0,0 +1,258 @@
+package com.ssafy.neegongnaegong.presentation.calendar.edit
+
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Description
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Repeat
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.ssafy.neegongnaegong.domain.model.calendar.RepeatRuleInfo
+import com.ssafy.neegongnaegong.domain.model.calendar.RepeatType
+import com.ssafy.neegongnaegong.domain.model.calendar.Schedule
+import com.ssafy.neegongnaegong.domain.model.calendar.UpdateType
+import com.ssafy.neegongnaegong.presentation.calendar.component.RepeatRuleInput
+import com.ssafy.neegongnaegong.presentation.calendar.component.ScheduleEditText
+import com.ssafy.neegongnaegong.presentation.calendar.component.picker.DateTimeRangePicker
+import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+
+@Composable
+fun ScheduleEditRoute(
+    modifier: Modifier = Modifier,
+    schedule: Schedule,
+    viewModel: ScheduleEditViewModel = hiltViewModel(),
+    popBackStack: () -> Unit,
+) {
+    BackHandler {
+        popBackStack()
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.setEvent(ScheduleEditContract.Event.InitSchedule(schedule))
+    }
+
+    val uiState = viewModel.uiState.collectAsState()
+
+    ScheduleEditContent(
+        modifier = modifier,
+        effect = viewModel.effect,
+        uiState = uiState.value,
+        onTitleChanged = { viewModel.setEvent(ScheduleEditContract.Event.OnTitleChanged(it)) },
+        onContentChanged = { viewModel.setEvent(ScheduleEditContract.Event.OnContentChanged(it)) },
+        onStartDateChanged = { viewModel.setEvent(ScheduleEditContract.Event.OnStartDateChanged(it)) },
+        onEndDateChanged = { viewModel.setEvent(ScheduleEditContract.Event.OnEndDateChanged(it)) },
+        onLocationChanged = { viewModel.setEvent(ScheduleEditContract.Event.OnLocationChanged(it)) },
+        onRepeatRuleChanged = { viewModel.setEvent(ScheduleEditContract.Event.OnRepeatRuleChanged(it)) },
+        onSaveScheduleClicked = {
+            viewModel.setEvent(
+                ScheduleEditContract.Event.OnSaveScheduleClicked(
+                    it
+                )
+            )
+        },
+        onCancelClick = { viewModel.setEvent(ScheduleEditContract.Event.OnCancelClick) }
+    )
+}
+
+@Composable
+fun ScheduleEditContent(
+    modifier: Modifier = Modifier,
+    effect: Flow<ScheduleEditContract.Effect>,
+    uiState: ScheduleEditContract.State,
+    onTitleChanged: (String) -> Unit,
+    onContentChanged: (String) -> Unit,
+    onStartDateChanged: (LocalDateTime) -> Unit,
+    onEndDateChanged: (LocalDateTime) -> Unit,
+    onLocationChanged: (String) -> Unit,
+    onRepeatRuleChanged: (RepeatRuleInfo?) -> Unit,
+    onSaveScheduleClicked: (UpdateType) -> Unit,
+    onCancelClick: () -> Unit,
+) {
+    val backDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(effect) {
+        effect.collectLatest { effect ->
+            when (effect) {
+                ScheduleEditContract.Effect.NavigateBack -> backDispatcher?.onBackPressed()
+                is ScheduleEditContract.Effect.ShowErrorSnackBar -> scope.launch {
+                    snackbarHostState.showSnackbar(
+                        message = effect.message,
+                        actionLabel = "확인",
+                        duration = SnackbarDuration.Short
+                    )
+                }
+            }
+        }
+    }
+
+    if (uiState.schedule == null) return
+
+    ScheduleEditScreen(
+        modifier = modifier,
+        title = uiState.schedule.title,
+        content = uiState.schedule.content,
+        startDate = uiState.schedule.startDate,
+        endDate = uiState.schedule.endDate,
+        location = uiState.schedule.location,
+        repeatRule = uiState.repeatRule,
+        onTitleChange = onTitleChanged,
+        onContentChange = onContentChanged,
+        onRepeatRuleChanged = onRepeatRuleChanged,
+        onStartDateChange = onStartDateChanged,
+        onEndDateChange = onEndDateChanged,
+        onLocationChange = onLocationChanged,
+        onSaveScheduleClicked = onSaveScheduleClicked,
+        onCancelClick = onCancelClick
+    )
+}
+
+@Composable
+fun ScheduleEditScreen(
+    modifier: Modifier = Modifier,
+    title: String,
+    content: String? = null,
+    startDate: LocalDateTime,
+    endDate: LocalDateTime,
+    location: String?,
+    repeatRule: RepeatRuleInfo?,
+    onTitleChange: (String) -> Unit,
+    onContentChange: (String) -> Unit,
+    onLocationChange: (String) -> Unit,
+    onRepeatRuleChanged: (RepeatRuleInfo?) -> Unit,
+    onStartDateChange: (LocalDateTime) -> Unit,
+    onEndDateChange: (LocalDateTime) -> Unit,
+    onSaveScheduleClicked: (UpdateType) -> Unit,
+    onCancelClick: () -> Unit,
+) {
+    var isRepeatRuleFocused by remember { mutableStateOf(false) }
+
+    Column(modifier = Modifier.fillMaxSize().then(modifier)) {
+        Column(modifier = Modifier.verticalScroll(rememberScrollState()).padding(vertical = 16.dp).weight(1f)) {
+            ScheduleEditText(
+                modifier = Modifier.fillMaxWidth(),
+                text = title,
+                onTextChange = onTitleChange,
+                placeHolder = "제목"
+            )
+            DateTimeRangePicker(
+                startDateTime = startDate,
+                endDateTime = endDate,
+                onStartDateTimeChange = onStartDateChange,
+                onEndDateTimeChange = onEndDateChange,
+            )
+            ScheduleEditText(
+                modifier = Modifier.fillMaxWidth(),
+                text = content ?: "",
+                onTextChange = onContentChange,
+                placeHolder = "메모",
+                prefix = Icons.Outlined.Description,
+            )
+            ScheduleEditText(
+                modifier = Modifier.fillMaxWidth(),
+                text = location ?: "",
+                onTextChange = onLocationChange,
+                placeHolder = "장소",
+                prefix = Icons.Outlined.LocationOn,
+            )
+            ScheduleEditText(
+                modifier = Modifier.fillMaxWidth().clickable { isRepeatRuleFocused = !isRepeatRuleFocused },
+                text = repeatRule?.toDisplayString() ?: "반복 안 함",
+                placeHolder = "반복 안 함",
+                prefix = Icons.Outlined.Repeat,
+                enabled = false,
+            )
+            AnimatedVisibility(isRepeatRuleFocused) {
+                RepeatRuleInput(
+                    repeatRule = repeatRule,
+                    onRepeatRuleChange = onRepeatRuleChanged
+                )
+            }
+        }
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            TextButton(
+                onClick = onCancelClick,
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    "취소",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
+            TextButton(
+                onClick = { onSaveScheduleClicked(UpdateType.ALL) },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    "확인",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewScheduleEditScreen() {
+    NeeGongNaeGongTheme(dynamicColor = false) {
+        Surface {
+            ScheduleEditScreen(
+                modifier = Modifier.fillMaxSize(),
+                title = "New Schedule",
+                content = null,
+                startDate = LocalDateTime.now(),
+                endDate = LocalDateTime.now().plusHours(1),
+                location = null,
+                repeatRule = RepeatRuleInfo(
+                    repeatType = RepeatType.MONTHLY,
+                    repeatInterval = 1,
+                    repeatDay = 3,
+                    endDate = null
+                ),
+                onTitleChange = { },
+                onContentChange = { },
+                onLocationChange = { },
+                onRepeatRuleChanged = { },
+                onStartDateChange = { },
+                onEndDateChange = { },
+                onSaveScheduleClicked = { },
+                onCancelClick = { }
+            )
+        }
+    }
+}
