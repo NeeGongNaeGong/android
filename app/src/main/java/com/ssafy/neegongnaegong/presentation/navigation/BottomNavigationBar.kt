769196e9f8be4a18d13145f132efa377752997f4
전형선 <hsun5163@gmail.com>
2025-03-28 18:26:35 +0900

    ui(#18): BottomNavigationBar UI 구현
    커스텀화 : 클릭시 파장효과 제거, 선택된 NavItem 배경 제거

---
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/navigation/BottomNavigationBar.kt
@@ -0,0 +1,116 @@
+package com.ssafy.neegongnaegong.presentation.navigation
+
+import androidx.compose.foundation.interaction.Interaction
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.ssafy.neegongnaegong.presentation.ui.theme.LightColors.BackGround
+import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongTheme
+import com.ssafy.neegongnaegong.presentation.ui.theme.Typography
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+@Composable
+fun BottomNavigationBar(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+) {
+    val screens =
+        listOf(
+            BottomNavItem.StudiesScreen,
+            BottomNavItem.PersonalScreen,
+            BottomNavItem.CalendarScreen,
+            BottomNavItem.ProfileScreen,
+        )
+
+    NavigationBar(
+        modifier =
+            modifier
+                .fillMaxHeight(0.09f)
+                .navigationBarsPadding()
+                .shadow(
+                    elevation = 20.dp,
+                    shape = RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp),
+                ),
+        containerColor = BackGround,
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        screens.forEach { screen ->
+            val selected = currentRoute == screen.route
+
+            NavigationBarItem(
+                interactionSource = NoRippleInteractionSource,
+                icon = {
+                    Icon(
+                        painter =
+                            painterResource(
+                                id = if (currentRoute == screen.route) screen.iconSelected else screen.icon,
+                            ),
+                        contentDescription = stringResource(id = screen.title),
+                        tint = Color.Unspecified,
+                    )
+                },
+                label = {
+                    Text(
+                        text = stringResource(id = screen.title),
+                        style = if (selected) Typography.bodySmall.copy(fontSize = 12.sp) else Typography.labelSmall,
+                    )
+                },
+                selected = currentRoute == screen.route,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(indicatorColor = Color.Transparent),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, heightDp = 70)
+@Composable
+fun PreviewBottomNavigationBar() {
+    NeeGongNaeGongTheme {
+        BottomNavigationBar(
+            navController = rememberNavController(),
+        )
+    }
+}
+
+/**
+ * 클릭시 알약 파장 효과를 제거.
+ */
+private object NoRippleInteractionSource : MutableInteractionSource {
+    override val interactions: Flow<Interaction> = emptyFlow()
+
+    override suspend fun emit(interaction: Interaction) {}
+
+    override fun tryEmit(interaction: Interaction) = true
+}
