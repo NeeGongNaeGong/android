2d3af79083583703ec8fc4d34745a5f64e9a0555
박준식 <akfldkskgorn@gmail.com>
2025-03-28 23:23:22 +0900

    feat(#20): AuthInterceptor 구현

---
+++ app/src/main/java/com/ssafy/neegongnaegong/data/remote/interceptor/AuthInterceptor.kt
@@ -0,0 +1,22 @@
+package com.ssafy.neegongnaegong.data.remote.interceptor
+
+import com.ssafy.neegongnaegong.BuildConfig
+import com.ssafy.neegongnaegong.data.remote.TokenManager
+import com.ssafy.neegongnaegong.domain.exception.AuthException
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val tokenManager: TokenManager,
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
+        val token = tokenManager.getToken(BuildConfig.TOKEN_TYPE_ACCESS) ?: throw AuthException.InvalidTokenException()
+
+        val request = request().newBuilder()
+            .addHeader("Authorization", "Bearer $token")
+            .build()
+
+        proceed(request)
+    }
+}
