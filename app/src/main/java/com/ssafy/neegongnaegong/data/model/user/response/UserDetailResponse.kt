724a331c6f70b566def40e7b6bf5adbe73fc888c
박준식 <akfldkskgorn@gmail.com>
2025-03-29 17:21:00 +0900

    feat(#20): UserDetailResponse toDomain 추가

--- app/src/main/java/com/ssafy/neegongnaegong/data/model/user/response/UserDetailResponse.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/model/user/response/UserDetailResponse.kt
@@ -1,8 +1,17 @@
 package com.ssafy.neegongnaegong.data.model.user.response
 
+import com.ssafy.neegongnaegong.domain.model.User
+
 data class UserDetailResponse(
     val id: Long,
     val email: String,
     val nickname: String,
     val profileImg: String,
-)
+) {
+    fun toDomain() = User(
+        id = id,
+        email = email,
+        nickname = nickname,
+        profileImg = profileImg
+    )
+}
