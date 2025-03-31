55080eeccb3113b98b57f68669786ffa2f82d6bf
박준식 <akfldkskgorn@gmail.com>
2025-03-31 11:54:55 +0900

    feat(#20): network user datasource 구현 및 hilt 적용

--- app/src/main/java/com/ssafy/neegongnaegong/data/datasource/network/NetworkUserDataSource.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/datasource/network/NetworkUserDataSource.kt
@@ -1,11 +1,12 @@
 package com.ssafy.neegongnaegong.data.datasource.network
 
+import com.ssafy.neegongnaegong.data.model.user.request.UpdateUserRequest
 import com.ssafy.neegongnaegong.data.model.user.response.UserDetailResponse
+import com.ssafy.neegongnaegong.data.model.user.response.ValidateNicknameResponse
 import kotlinx.coroutines.flow.Flow
 
 interface NetworkUserDataSource {
-    suspend fun getUser(): Flow<UserDetailResponse>
-    suspend fun validateUserNickname(nickname: String): Flow<Boolean>
-    suspend fun updateUserNickname(nickname: String): Flow<UserDetailResponse>
-    suspend fun updateUserProfileImg(image: String): Flow<UserDetailResponse>
+    suspend fun getUser(id: Long): Flow<UserDetailResponse>
+    suspend fun validateUserNickname(nickname: String): Flow<ValidateNicknameResponse>
+    suspend fun updateUser(request: UpdateUserRequest): Flow<Unit>
 }
