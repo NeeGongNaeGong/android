f3fb08e0ca7fb2306c7c3552083663c0df360440
박준식 <akfldkskgorn@gmail.com>
2025-04-04 11:05:11 +0900

    feat(#17): Schedule Detail Contract 구현

---
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/calendar/detail/ScheduleDetailContract.kt
@@ -0,0 +1,30 @@
+package com.ssafy.neegongnaegong.presentation.calendar.detail
+
+import com.ssafy.neegongnaegong.domain.model.calendar.DeleteType
+import com.ssafy.neegongnaegong.domain.model.calendar.Schedule
+import com.ssafy.neegongnaegong.presentation.base.UiEffect
+import com.ssafy.neegongnaegong.presentation.base.UiEvent
+import com.ssafy.neegongnaegong.presentation.base.UiState
+import java.time.LocalDate
+
+class ScheduleDetailContract {
+    sealed class Event : UiEvent {
+        data class OnLoad(val date: LocalDate, val schedule: Schedule) : Event()
+        data object OnEditClick : Event()
+        data class OnDeleteClick(val type: DeleteType) : Event()
+    }
+
+    data class State(
+        val isLoading: Boolean = false,
+        val isSuccess: Boolean = false,
+        val isFailure: Boolean = false,
+        val date: LocalDate? = null,
+        val schedule: Schedule? = null,
+    ) : UiState
+
+    sealed class Effect : UiEffect {
+        data object NavigateBack : Effect()
+        data class NavigateToEditScheduleScreen(val schedule: Schedule) : Effect()
+        data class ShowErrorSnackBar(val message: String) : Effect()
+    }
+}
