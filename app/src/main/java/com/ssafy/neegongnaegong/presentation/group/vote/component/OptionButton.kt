5f87c33b013fee23470250d77acf5db29eb5e162
Na2te <111sym@naver.com>
2025-04-14 19:03:59 +0900

    feat(#36): Option Item 수정 - 테마 설정으로 불필요한 indicator 관련 설정 수정

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/group/vote/component/OptionButton.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/group/vote/component/OptionButton.kt
@@ -1,33 +1,48 @@
 package com.ssafy.neegongnaegong.presentation.group.vote.component
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Circle
-import androidx.compose.material.icons.outlined.Circle
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun OptionButton( isSelected: Boolean, optionTitle: String, modifier : Modifier = Modifier, onClick: () -> Unit,) {
-    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
-        IconButton(onClick = onClick) {
-            Icon(
-                imageVector = if(isSelected)Icons.Filled.Circle else Icons.Outlined.Circle,
-                contentDescription = "선택 여부"
-            )
-        }
-        Text(optionTitle)
-    }
+fun OptionButton(
+    isSelected: Boolean,
+    optionTitle: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides 0.dp) {
+        Row(
+            modifier = modifier.padding(0.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
 
+        ) {
+            RadioButton(
+                selected = isSelected,
+                onClick = onClick,
+            )
+            Text(optionTitle)
+        }
+    }
 }
 
 @Preview
 @Composable
-fun PreviewOptionButton(){
-    OptionButton(false, "종료 시간"){}
+fun PreviewOptionButton() {
+    OptionButton(false, "종료 시간") {}
 }
