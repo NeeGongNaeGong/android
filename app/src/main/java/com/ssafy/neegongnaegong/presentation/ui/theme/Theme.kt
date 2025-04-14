c2b59e6f2b984d799b61c42b5253995d9d938d53
Na2te <111sym@naver.com>
2025-04-14 18:59:07 +0900

    feat(#36): Compose에서 클릭 관련 Ripple Theme 나오는 것 비활성화 처리 - Gradle에서 Material3 최신 버전으로 설정

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/ui/theme/Theme.kt
@@ -2,12 +2,15 @@ package com.ssafy.neegongnaegong.presentation.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalRippleConfiguration
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
 
 
@@ -37,6 +40,7 @@ private val DarkColorScheme = darkColorScheme(
     secondaryContainer = DarkColors.DarkGray4,
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NeeGongNaeGongTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -53,9 +57,12 @@ fun NeeGongNaeGongTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+
+    CompositionLocalProvider (LocalRippleConfiguration provides null) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 }
