c7d41b131024aec5a8a4da559bfa77e7b076a0c9
김민조 <upsk1@naver.com>
2025-04-15 16:51:47 +0900

    feat(#41): 날짜 내림차순 별 StudyRecord 표시

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/personal/component/StudyRecordList.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/personal/component/StudyRecordList.kt
@@ -1,25 +1,48 @@
 package com.ssafy.neegongnaegong.presentation.personal.component
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ssafy.neegongnaegong.domain.model.personal.StudyRecord
+import com.ssafy.neegongnaegong.presentation.ui.theme.Typography
+import com.ssafy.neegongnaegong.presentation.util.toDateString
 
 @Composable
 fun StudyRecordList(
     modifier: Modifier = Modifier,
     studyRecords: List<StudyRecord>,
 ) {
+
+    val groupedRecords = studyRecords
+        .sortedByDescending { it.startTime }
+        .groupBy { it.startTime.toDateString() }
+
+
     LazyColumn(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        items(studyRecords) { record ->
-            StudyRecordItem(record)
+        groupedRecords.forEach { (date, recordsForDate) ->
+            item {
+                Text(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    text = date,
+                    style = Typography.bodySmall.copy(
+                        fontSize = 20.sp
+                    ),
+                )
+            }
+
+            items(recordsForDate) { record ->
+                StudyRecordItem(record)
+            }
         }
     }
 }
