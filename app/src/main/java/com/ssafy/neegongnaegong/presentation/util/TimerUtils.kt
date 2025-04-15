796d14822e491072ca545b0970e1c2a59996ea28
김민조 <upsk1@naver.com>
2025-04-15 13:53:23 +0900

    feat(#41): ISO 8601 시간 Util 생성

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/util/TimerUtils.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/util/TimerUtils.kt
@@ -1,5 +1,7 @@
 package com.ssafy.neegongnaegong.presentation.util
 
+import java.time.Instant
+import java.time.ZoneId
 import java.util.Locale
 
 fun formatElapsedTime(elapsedTime: Long): String {
@@ -9,3 +11,33 @@ fun formatElapsedTime(elapsedTime: Long): String {
 
     return String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds)
 }
+
+
+fun String.toDateString(): String {
+    return try {
+        val instant = Instant.parse(this)
+        val localDate = instant.atZone(ZoneId.systemDefault()).toLocalDate()
+        "%04d.%02d.%02d".format(localDate.year, localDate.monthValue, localDate.dayOfMonth)
+    } catch (e: Exception) {
+        "잘못된 날짜 형식"
+    }
+}
+
+fun String.toTimeString(): String {
+    return try {
+        val instant = Instant.parse(this)
+        val localTime = instant.atZone(ZoneId.systemDefault()).toLocalTime()
+        val hour = localTime.hour
+        val minute = localTime.minute
+
+        val isAm = hour < 12
+        val hour12 = if (hour % 12 == 0) 12 else hour % 12
+        val amPm = if (isAm) "오전" else "오후"
+
+        "$amPm ${hour12}시 ${minute}분"
+    } catch (e: Exception) {
+        "잘못된 날짜 형식"
+    }
+}
+
+
