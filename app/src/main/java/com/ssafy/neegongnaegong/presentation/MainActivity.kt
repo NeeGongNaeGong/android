c90e68565b7cd485e0b24177ff6c14a9d85f1ffc
박준식 <akfldkskgorn@gmail.com>
2025-04-19 15:55:39 +0900

    feat(#39): 권한 요청 Manager 구현

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/MainActivity.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/MainActivity.kt
@@ -4,18 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ssafy.neegongnaegong.presentation.splash.SplashViewModel
 import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongTheme
+import com.ssafy.neegongnaegong.presentation.util.PermissionManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     val viewModel: SplashViewModel by viewModels()
+    
+    private val permissions = PermissionManager.getPermissions()
+
+    private val permissionLauncher by lazy {
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { _ -> }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        PermissionManager.requestPermissions(this, permissionLauncher, permissions)
 
         val splashScreen = installSplashScreen()
         splashScreen.setKeepOnScreenCondition { viewModel.isLoading }
