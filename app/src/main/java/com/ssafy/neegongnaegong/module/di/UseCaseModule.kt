5fd6177c4b3b5bf5099a315a2fee5b5ec2fbd740
김민조 <upsk1@naver.com>
2025-04-28 13:14:39 +0900

    feat(#57): 유즈케이스 모듈 추가

--- app/src/main/java/com/ssafy/neegongnaegong/module/di/UseCaseModule.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/module/di/UseCaseModule.kt
@@ -1,9 +1,26 @@
 package com.ssafy.neegongnaegong.module.di
 
+import com.ssafy.neegongnaegong.domain.repository.AuthRepository
+import com.ssafy.neegongnaegong.domain.repository.CalendarRepository
 import com.ssafy.neegongnaegong.domain.repository.GitHubRepository
 import com.ssafy.neegongnaegong.domain.repository.StudiesRepository
+import com.ssafy.neegongnaegong.domain.repository.UserRepository
 import com.ssafy.neegongnaegong.domain.usecase.GetStudiesUseCase
 import com.ssafy.neegongnaegong.domain.usecase.GetUserReposUseCase
+import com.ssafy.neegongnaegong.domain.usecase.auth.LoginUseCase
+import com.ssafy.neegongnaegong.domain.usecase.auth.LogoutUseCase
+import com.ssafy.neegongnaegong.domain.usecase.auth.RegisterUseCase
+import com.ssafy.neegongnaegong.domain.usecase.auth.ReissueUseCase
+import com.ssafy.neegongnaegong.domain.usecase.calendar.CreatePersonalSchedulesUseCase
+import com.ssafy.neegongnaegong.domain.usecase.calendar.DeletePersonalSchedulesUseCase
+import com.ssafy.neegongnaegong.domain.usecase.calendar.GetScheduleDetailUseCase
+import com.ssafy.neegongnaegong.domain.usecase.calendar.GetUserSchedulesUseCase
+import com.ssafy.neegongnaegong.domain.usecase.calendar.UpdatePersonalSchedulesUseCase
+import com.ssafy.neegongnaegong.domain.usecase.user.GetCurrentUserUseCase
+import com.ssafy.neegongnaegong.domain.usecase.user.GetUserUseCase
+import com.ssafy.neegongnaegong.domain.usecase.user.UpdateNicknameUseCase
+import com.ssafy.neegongnaegong.domain.usecase.user.UpdateProfileImageUseCase
+import com.ssafy.neegongnaegong.domain.usecase.user.ValidateNicknameUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,11 +31,88 @@ import dagger.hilt.android.scopes.ViewModelScoped
 @InstallIn(ViewModelComponent::class)
 object UseCaseModule {
 
+    // Auth UseCases
     @Provides
     @ViewModelScoped
-    fun provideGetUserReposUseCase(repository: GitHubRepository): GetUserReposUseCase = GetUserReposUseCase(repository)
+    fun provideLoginUseCase(authRepository: AuthRepository): LoginUseCase =
+        LoginUseCase(authRepository)
 
     @Provides
     @ViewModelScoped
-    fun provideGetGroupsUseCase(repository: StudiesRepository): GetStudiesUseCase = GetStudiesUseCase(repository)
+    fun provideLogoutUseCase(authRepository: AuthRepository): LogoutUseCase =
+        LogoutUseCase(authRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideReissueUseCase(authRepository: AuthRepository): ReissueUseCase =
+        ReissueUseCase(authRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideRegisterUseCase(authRepository: AuthRepository): RegisterUseCase =
+        RegisterUseCase(authRepository)
+
+    // Calendar UseCases
+    @Provides
+    @ViewModelScoped
+    fun provideCreatePersonalSchedulesUseCase(calendarRepository: CalendarRepository): CreatePersonalSchedulesUseCase =
+        CreatePersonalSchedulesUseCase(calendarRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideDeletePersonalSchedulesUseCase(calendarRepository: CalendarRepository): DeletePersonalSchedulesUseCase =
+        DeletePersonalSchedulesUseCase(calendarRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideGetScheduleDetailUseCase(calendarRepository: CalendarRepository): GetScheduleDetailUseCase =
+        GetScheduleDetailUseCase(calendarRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideGetUserSchedulesUseCase(calendarRepository: CalendarRepository): GetUserSchedulesUseCase =
+        GetUserSchedulesUseCase(calendarRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideUpdatePersonalSchedulesUseCase(calendarRepository: CalendarRepository): UpdatePersonalSchedulesUseCase =
+        UpdatePersonalSchedulesUseCase(calendarRepository)
+
+    // User UseCases
+    @Provides
+    @ViewModelScoped
+    fun provideGetCurrentUserUseCase(userRepository: UserRepository): GetCurrentUserUseCase =
+        GetCurrentUserUseCase(userRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideGetUserUseCase(userRepository: UserRepository): GetUserUseCase =
+        GetUserUseCase(userRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideUpdateNicknameUseCase(userRepository: UserRepository): UpdateNicknameUseCase =
+        UpdateNicknameUseCase(userRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideUpdateProfileImageUseCase(userRepository: UserRepository): UpdateProfileImageUseCase =
+        UpdateProfileImageUseCase(userRepository)
+
+    @Provides
+    @ViewModelScoped
+    fun provideValidateNicknameUseCase(userRepository: UserRepository): ValidateNicknameUseCase =
+        ValidateNicknameUseCase(userRepository)
+
+    // Study UseCase
+    @Provides
+    @ViewModelScoped
+    fun provideGetStudiesUseCase(studiesRepository: StudiesRepository): GetStudiesUseCase =
+        GetStudiesUseCase(studiesRepository)
+
+    // GitHub UseCase
+    @Provides
+    @ViewModelScoped
+    fun provideGetUserReposUseCase(gitHubRepository: GitHubRepository): GetUserReposUseCase =
+        GetUserReposUseCase(gitHubRepository)
 }
