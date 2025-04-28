6725ab9f37f5af2f93dcc614909b815c0327a9e7
박준식 <akfldkskgorn@gmail.com>
2025-04-29 00:10:34 +0900

    refactor(#53): NeeGongNaeGongSnackbar 컴포넌트 분리

---
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/component/snackbar/EmojiText.kt
@@ -0,0 +1,14 @@
+package com.ssafy.neegongnaegong.presentation.component.snackbar
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongTheme
+
+@Composable
+internal fun EmojiText(emoji: String?) {
+    if (emoji != null) Text(
+        emoji,
+        color = NeeGongNaeGongTheme.colorScheme.primaryText,
+        style = NeeGongNaeGongTheme.typography.titleSmall
+    )
+}
--- app/src/main/java/com/ssafy/neegongnaegong/presentation/component/snackbar/NeeGongNaeGongSnackbarHost.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/component/snackbar/NeeGongNaeGongSnackbarHost.kt
@@ -1,31 +1,16 @@
 package com.ssafy.neegongnaegong.presentation.component.snackbar
 
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ssafy.neegongnaegong.presentation.base.SnackbarViewModel
+import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongPreviews
 import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongTheme
 import com.ssafy.neegongnaegong.presentation.util.SnackbarManager
-import kotlinx.coroutines.launch
-import kotlin.math.hypot
 
 @Composable
 fun NeeGongNaeGongSnackbarHost() {
@@ -42,61 +27,93 @@ fun NeeGongNaeGongSnackbarHost() {
 
     SnackbarHost(hostState = snackbarHostState) {
         val visuals = it.visuals as? NeeGongNaeGongSnackbarVisuals ?: return@SnackbarHost
-        val (message, type) = visuals
-        val backgroundColor = when (type) {
-            SnackbarManager.Type.Success -> NeeGongNaeGongTheme.colorScheme.gray3
-            SnackbarManager.Type.Warning -> NeeGongNaeGongTheme.colorScheme.lightGreen
-            SnackbarManager.Type.Error -> NeeGongNaeGongTheme.colorScheme.peach
-            SnackbarManager.Type.None -> NeeGongNaeGongTheme.colorScheme.mint
-        }
-
-        Card(
-            modifier = Modifier
-                .dismissWithDragGestures { snackbarHostState.currentSnackbarData?.dismiss() }
-                .padding(16.dp)
-                .fillMaxWidth(),
-            colors = CardDefaults.cardColors(containerColor = backgroundColor)
-        ) {
-            Text(
-                modifier = Modifier.padding(16.dp),
-                text = message,
-                color = NeeGongNaeGongTheme.colorScheme.primaryText,
-            )
-        }
+        NeeGongNaeGongSnackbarWithVisuals(
+            modifier = Modifier.dismissWithDragGestures {
+                snackbarHostState.currentSnackbarData?.dismiss()
+            },
+            visuals = visuals,
+        )
     }
 }
 
 @Composable
-fun Modifier.dismissWithDragGestures(onDismiss: suspend () -> Unit): Modifier {
-    val scope = rememberCoroutineScope()
+fun NeeGongNaeGongSnackbarWithVisuals(
+    modifier: Modifier = Modifier,
+    visuals: NeeGongNaeGongSnackbarVisuals
+) {
+    val (message, type) = visuals
 
-    var offsetY by remember { mutableFloatStateOf(0f) }
-    var offsetX by remember { mutableFloatStateOf(0f) }
+    val backgroundColor = when (type) {
+        SnackbarManager.Type.None -> NeeGongNaeGongTheme.colorScheme.gray1
+        SnackbarManager.Type.Success -> NeeGongNaeGongTheme.colorScheme.mint
+        SnackbarManager.Type.Warning -> NeeGongNaeGongTheme.colorScheme.lightGreen
+        SnackbarManager.Type.Error -> NeeGongNaeGongTheme.colorScheme.peach
+    }
 
-    return this
-        .offset { IntOffset(x = offsetX.toInt(), y = offsetY.toInt()) }
-        .pointerInput(Unit) {
-            detectDragGestures(
-                onDrag = { change, dragAmount ->
-                    change.consume()
+    val prefixEmoji = when (type) {
+        SnackbarManager.Type.None -> null
+        SnackbarManager.Type.Success -> "✓"
+        SnackbarManager.Type.Warning -> "⚠"
+        SnackbarManager.Type.Error -> "ⓘ"
+    }
 
-                    offsetX += dragAmount.x
-                    offsetY += dragAmount.y
+    NeeGongNaeGongSnackbar(
+        modifier = modifier,
+        message = message,
+        backgroundColor = backgroundColor,
+        prefix = { EmojiText(prefixEmoji) }
+    )
+}
 
-                    val dragDistance = hypot(offsetX, offsetY)
 
-                    // 어느 방향으로든 충분히 끌면 dismiss
-                    if (dragDistance > 50f) {
-                        scope.launch {
-                            onDismiss()
-                        }
-                    }
-                },
-                onDragEnd = {
-                    // 드래그 끝났는데 dismiss 안 했으면 원위치
-                    offsetX = 0f
-                    offsetY = 0f
-                }
+@NeeGongNaeGongPreviews
+@Composable
+fun NeeGongNaeGongSnackbarWithVisualsPreview_None() {
+    NeeGongNaeGongTheme {
+        NeeGongNaeGongSnackbarWithVisuals(
+            visuals = NeeGongNaeGongSnackbarVisuals(
+                message = "일반 메시지",
+                type = SnackbarManager.Type.None
             )
-        }
+        )
+    }
+}
+
+@NeeGongNaeGongPreviews
+@Composable
+fun NeeGongNaeGongSnackbarWithVisualsPreview_Success() {
+    NeeGongNaeGongTheme {
+        NeeGongNaeGongSnackbarWithVisuals(
+            visuals = NeeGongNaeGongSnackbarVisuals(
+                message = "성공 메시지",
+                type = SnackbarManager.Type.Success
+            )
+        )
+    }
+}
+
+@NeeGongNaeGongPreviews
+@Composable
+fun NeeGongNaeGongSnackbarWithVisualsPreview_Warning() {
+    NeeGongNaeGongTheme {
+        NeeGongNaeGongSnackbarWithVisuals(
+            visuals = NeeGongNaeGongSnackbarVisuals(
+                message = "경고 메시지",
+                type = SnackbarManager.Type.Warning
+            )
+        )
+    }
+}
+
+@NeeGongNaeGongPreviews
+@Composable
+fun NeeGongNaeGongSnackbarWithVisualsPreview_Error() {
+    NeeGongNaeGongTheme {
+        NeeGongNaeGongSnackbarWithVisuals(
+            visuals = NeeGongNaeGongSnackbarVisuals(
+                message = "에러 메시지",
+                type = SnackbarManager.Type.Error
+            )
+        )
+    }
 }
