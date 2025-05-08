161ecf9fe59b9c796ac08a2ca5bc086152460544
Na2te <111sym@naver.com>
2025-05-08 11:13:28 +0900

    fix(#66): 로그인 에러 수정 - UserDetailResponse의 nickname이 camel case가 아니라 발생하는 에러 수정

--- app/src/main/java/com/ssafy/neegongnaegong/data/model/user/response/UserDetailResponse.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/model/user/response/UserDetailResponse.kt
@@ -1,10 +1,12 @@
 package com.ssafy.neegongnaegong.data.model.user.response
 
+import com.google.gson.annotations.SerializedName
 import com.ssafy.neegongnaegong.domain.model.User
 
 data class UserDetailResponse(
     val id: Long,
     val email: String,
+    @SerializedName("nickName")
     val nickname: String,
     val profileImg: String,
 ) {
