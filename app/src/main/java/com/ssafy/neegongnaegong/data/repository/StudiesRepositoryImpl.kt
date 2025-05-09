c0755e8422b9dd8bd5eb0d237c4952dd83b91857
Na2te <111sym@naver.com>
2025-05-09 16:47:25 +0900

    fix(#69): StudiesRepository, StudiesRepositoryImpl 수정 - getStudies가 깡통 API고, DataSource가 아니라 Api를 직접 참조하고 있어서 주석 처리를 했으나 TODO로 변경

--- app/src/main/java/com/ssafy/neegongnaegong/data/repository/StudiesRepositoryImpl.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/repository/StudiesRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.ssafy.neegongnaegong.data.repository
 
 import com.ssafy.neegongnaegong.data.datasource.network.NetworkStudiesDataSource
 import com.ssafy.neegongnaegong.data.model.studies.request.CreateVoteRequest
+import com.ssafy.neegongnaegong.domain.model.studies.Studies
 import com.ssafy.neegongnaegong.domain.model.studies.VoteInfo
 import com.ssafy.neegongnaegong.domain.repository.StudiesRepository
 import kotlinx.coroutines.flow.Flow
@@ -12,7 +13,7 @@ class StudiesRepositoryImpl
 constructor(
     private val dataSource: NetworkStudiesDataSource,
 ) : StudiesRepository {
-//    override suspend fun getStudies(): List<Studies> = api.getStudies()
+    override suspend fun getStudies(): List<Studies> = TODO()
 
     override suspend fun createVote(studyId: Int, voteInfo: VoteInfo): Flow<Unit> =
         dataSource.createVote(studyId, CreateVoteRequest.fromDomain(voteInfo))
--- app/src/main/java/com/ssafy/neegongnaegong/domain/repository/StudiesRepository.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/domain/repository/StudiesRepository.kt
@@ -1,10 +1,11 @@
 package com.ssafy.neegongnaegong.domain.repository
 
+import com.ssafy.neegongnaegong.domain.model.studies.Studies
 import com.ssafy.neegongnaegong.domain.model.studies.VoteInfo
 import kotlinx.coroutines.flow.Flow
 
 interface StudiesRepository {
-//    suspend fun getStudies(): List<Studies>
+    suspend fun getStudies(): List<Studies>
 
     suspend fun createVote(studyId: Int, voteInfo: VoteInfo): Flow<Unit>
 }
