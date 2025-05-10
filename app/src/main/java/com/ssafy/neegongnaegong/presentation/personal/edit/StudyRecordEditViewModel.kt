2922421a24c1b3bbe31732c94f5f408fa0634c10
김민조 <upsk1@naver.com>
2025-05-10 16:23:30 +0900

    feat(#8): 태그 표시

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/personal/edit/StudyRecordEditViewModel.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/personal/edit/StudyRecordEditViewModel.kt
@@ -132,6 +132,7 @@ class StudyRecordEditViewModel
                         setState {
                             copy(
                                 learningRecord = learningRecord,
+                                tags = learningRecord.tags,
                                 isLoading = false,
                             )
                         }
