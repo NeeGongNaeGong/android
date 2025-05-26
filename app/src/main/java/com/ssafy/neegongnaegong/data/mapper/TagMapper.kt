c34c32a36c8064c452e04985a42dead771470a0e
Aloe-droid <https://github.com/Aloe-droid>
2025-05-27 00:00:44 +0900

    refactor(#81): Tag Mapper 분리

--- app/src/main/java/com/ssafy/neegongnaegong/data/mapper/TagMapper.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/mapper/TagMapper.kt
@@ -1,6 +1,8 @@
 package com.ssafy.neegongnaegong.data.mapper
 
+import com.ssafy.neegongnaegong.data.model.learningrecord.response.TagResponse
 import com.ssafy.neegongnaegong.domain.data.TagData
+import com.ssafy.neegongnaegong.domain.model.learning.Tag
 
 object TagMapper {
     // id → 한글 이름
@@ -12,4 +14,10 @@ object TagMapper {
     val idToEnName: Map<Long, String> by lazy {
         TagData.tags.associate { it.id to it.enName }
     }
+
+    fun TagResponse.toDomain() = Tag(
+        id = id,
+        koName = name,
+        enName = idToEnName[id] ?: "English Name Not Found",
+    )
 }
