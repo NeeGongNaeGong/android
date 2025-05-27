38bbbfd7898bc7a4ac628741143702d4f0daaf04
Aloe-droid <https://github.com/Aloe-droid>
2025-05-27 21:18:04 +0900

    feat(#84): Notification Navigation Route 연결

---
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/notification/NotificationRoute.kt
@@ -0,0 +1,54 @@
+package com.ssafy.neegongnaegong.presentation.notification
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.ssafy.neegongnaegong.presentation.base.CollectSideEffects
+import com.ssafy.neegongnaegong.presentation.navigation.AppNavigation
+import com.ssafy.neegongnaegong.presentation.notification.data.NotificationUiModel
+import com.ssafy.neegongnaegong.presentation.util.SnackbarManager
+
+fun NavGraphBuilder.notificationScreen() = composable<AppNavigation.Screen.Profile.Notification> {
+    val viewModel: NotificationViewModel = hiltViewModel()
+    val listState: LazyListState = rememberLazyListState()
+    val uiState: NotificationContract.State by viewModel.uiState.collectAsStateWithLifecycle()
+    val notificationList: LazyPagingItems<NotificationUiModel> = viewModel
+        .notificationList
+        .collectAsLazyPagingItems()
+
+    CollectSideEffects(effectFlow = viewModel.effect) { effect: NotificationContract.Effect ->
+        when (effect) {
+            is NotificationContract.Effect.ShowErrorMessage -> {
+                SnackbarManager.showErrorMessage(message = effect.message)
+            }
+
+            is NotificationContract.Effect.ScrollToFirstPosition -> {
+                listState.animateScrollToItem(0)
+            }
+        }
+    }
+
+    NotificationScreen(
+        listState = listState,
+        isRefresh = uiState.isLoading,
+        notificationList = notificationList,
+        onRefresh = {
+            val event = NotificationContract.Event.RefreshEvent
+            viewModel.setEvent(event = event)
+        },
+        onDeleteAll = {
+            val event = NotificationContract.Event.DeleteAllNotification
+            viewModel.setEvent(event = event)
+        },
+        onDeleteNotification = { notificationUiModel: NotificationUiModel ->
+            val event = NotificationContract.Event.DeleteNotification(data = notificationUiModel)
+            viewModel.setEvent(event = event)
+        }
+    )
+}
