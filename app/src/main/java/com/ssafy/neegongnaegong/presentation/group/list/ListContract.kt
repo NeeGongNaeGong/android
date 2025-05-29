92bd890ece1772ec0ec7f58a59c40bbaae466bbb
Na2te <111sym@naver.com>
2025-05-29 12:35:52 +0900

    feat(#86): Contract 생성 - ListContract 생성 - TabIndex를 구분하기 위한 EnumClass 생성 - Tab 클릭 이벤트 처리 위한 Event와, 글 생성 이벤트 처리 위한 이벤트 생성

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/group/list/ListContract.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/group/list/ListContract.kt
@@ -4,11 +4,20 @@ import com.ssafy.neegongnaegong.presentation.base.UiEffect
 import com.ssafy.neegongnaegong.presentation.base.UiEvent
 import com.ssafy.neegongnaegong.presentation.base.UiState
 
+enum class Index(val index: Int, val title: String) {
+    Notice(0, "공지"),
+    Vote(1, "투표"),
+}
+
 class ListContract {
-    sealed class Event : UiEvent
+    sealed interface Event : UiEvent {
+        data object OnClickAddContent : Event
+
+        data class OnClickTab(val tab: Index) : Event
+    }
 
     data class State(
-        val index: Int,
+        val index: Index,
     ) : UiState
 
     sealed class Effect : UiEffect
