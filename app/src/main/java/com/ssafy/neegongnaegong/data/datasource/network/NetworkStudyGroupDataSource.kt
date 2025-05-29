a472080580ab70db32361f3e8a99add6ef499656
Na2te <111sym@naver.com>
2025-05-29 19:21:46 +0900

    feat(#86): NetworkStudyGroupDataSourceImpl 수정 - API를 통해 스터디별 투표 목록을 가져오는 함수 추가

--- app/src/main/java/com/ssafy/neegongnaegong/data/datasource/network/NetworkStudyGroupDataSource.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/datasource/network/NetworkStudyGroupDataSource.kt
@@ -2,8 +2,10 @@ package com.ssafy.neegongnaegong.data.datasource.network
 
 import com.ssafy.neegongnaegong.data.model.ApiResponse
 import com.ssafy.neegongnaegong.data.model.studygroup.response.MemberWeeklyStudyContentBySliceResponse
+import com.ssafy.neegongnaegong.data.model.studygroup.response.StudyGroupVoteListBySliceResponse
 import com.ssafy.neegongnaegong.data.model.studygroup.response.StudyLogByTagResponse
 import com.ssafy.neegongnaegong.domain.model.studygroup.MemberStudyContentsInfo
+import com.ssafy.neegongnaegong.domain.model.studygroup.StudyGroupVoteListInfo
 import com.ssafy.neegongnaegong.domain.model.studygroup.StudyMemberInfo
 import kotlinx.coroutines.flow.Flow
 
@@ -11,4 +13,6 @@ interface NetworkStudyGroupDataSource {
     fun getMemberStudyLogsByTag(request: StudyMemberInfo): Flow<List<StudyLogByTagResponse>>
 
     suspend fun getMemberStudyContents(request: MemberStudyContentsInfo): Result<ApiResponse<MemberWeeklyStudyContentBySliceResponse>>
+
+    suspend fun getStudyGroupVoteList(request: StudyGroupVoteListInfo): Result<ApiResponse<StudyGroupVoteListBySliceResponse>>
 }
--- app/src/main/java/com/ssafy/neegongnaegong/data/datasource/network/NetworkStudyGroupDataSourceImpl.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/datasource/network/NetworkStudyGroupDataSourceImpl.kt
@@ -3,28 +3,43 @@ package com.ssafy.neegongnaegong.data.datasource.network
 import com.ssafy.neegongnaegong.data.model.ApiResponse
 import com.ssafy.neegongnaegong.data.model.apiFlow
 import com.ssafy.neegongnaegong.data.model.studygroup.response.MemberWeeklyStudyContentBySliceResponse
+import com.ssafy.neegongnaegong.data.model.studygroup.response.StudyGroupVoteListBySliceResponse
 import com.ssafy.neegongnaegong.data.model.studygroup.response.StudyLogByTagResponse
 import com.ssafy.neegongnaegong.data.remote.StudyGroupApi
 import com.ssafy.neegongnaegong.domain.model.studygroup.MemberStudyContentsInfo
+import com.ssafy.neegongnaegong.domain.model.studygroup.StudyGroupVoteListInfo
 import com.ssafy.neegongnaegong.domain.model.studygroup.StudyMemberInfo
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class NetworkStudyGroupDataSourceImpl @Inject constructor(
-    private val api: StudyGroupApi
-) : NetworkStudyGroupDataSource {
-    override fun getMemberStudyLogsByTag(request: StudyMemberInfo): Flow<List<StudyLogByTagResponse>> =
-        apiFlow {
-            api.getMemberStudyLogsByTag(request.studyGroupId, request.targetUserId)
-        }
+class NetworkStudyGroupDataSourceImpl
+    @Inject
+    constructor(
+        private val api: StudyGroupApi,
+    ) : NetworkStudyGroupDataSource {
+        override fun getMemberStudyLogsByTag(request: StudyMemberInfo): Flow<List<StudyLogByTagResponse>> =
+            apiFlow {
+                api.getMemberStudyLogsByTag(request.studyGroupId, request.targetUserId)
+            }
 
-    override suspend fun getMemberStudyContents(request: MemberStudyContentsInfo): Result<ApiResponse<MemberWeeklyStudyContentBySliceResponse>> =
+        override suspend fun getMemberStudyContents(
+            request: MemberStudyContentsInfo,
+        ): Result<ApiResponse<MemberWeeklyStudyContentBySliceResponse>> =
             api.getMemberStudyContents(
                 request.studyGroupId,
                 request.userId,
                 request.cursorCreatedAt,
                 request.cursorId,
-                request.size
+                request.size,
             )
 
-}
+        override suspend fun getStudyGroupVoteList(
+            request: StudyGroupVoteListInfo,
+        ): Result<ApiResponse<StudyGroupVoteListBySliceResponse>> =
+            api.getStudyGroupVoteList(
+                studyGroupId = request.studyGroupId,
+                cursorTime = request.cursorTime,
+                cursorId = request.cursorId,
+                size = request.size,
+            )
+    }
