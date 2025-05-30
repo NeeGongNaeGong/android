7fc55ccc3c9803ed02997f245314894bc797a9a3
전형선 <hsun5163@gmail.com>
2025-05-30 12:55:23 +0900

    refactor(#60): dev 머지 후 변경 detekt 검사 후 통과된 파일
    - TimeFormatter renamed (클래스명과 멤버변수 네이밍이 동일)
    - Device Locale 적용

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/util/CustomDateTimeFormatter.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/util/CustomDateTimeFormatter.kt
@@ -1,14 +1,15 @@
 package com.ssafy.neegongnaegong.presentation.util
 
+import android.annotation.SuppressLint
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
-
-object TimeFormatter {
+object CustomDateTimeFormatter {
     private val dateFormatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 (E)")
     private val timeFormatter = DateTimeFormatter.ofPattern("a hh:mm")
 
@@ -18,8 +19,7 @@ object TimeFormatter {
      * @param LocalDateTime
      * @return String - yyyy년 MM월 dd일 (요일)
      */
-    fun convertLocalDateTimeToStringDate(time: LocalDateTime): String =
-        time.atZone(ZoneId.systemDefault()).format(dateFormatter)
+    fun convertLocalDateTimeToStringDate(time: LocalDateTime): String = time.atZone(ZoneId.systemDefault()).format(dateFormatter)
 
     /**
      * LocalDateTime.now 등으로 시간을 얻고
@@ -27,8 +27,7 @@ object TimeFormatter {
      * @param LocalDateTime
      * @return String - yyyy년 MM월 dd일 (요일)
      */
-    fun convertLocalDateTimeToStringTime(time: LocalDateTime): String =
-        time.atZone(ZoneId.systemDefault()).format(timeFormatter)
+    fun convertLocalDateTimeToStringTime(time: LocalDateTime): String = time.atZone(ZoneId.systemDefault()).format(timeFormatter)
 
     /**
      * yyyy년 MM월 dd일 (E) 꼴의 문자열을 인자로 넣으면 밀리초로 변환하는 함수
@@ -73,10 +72,31 @@ object TimeFormatter {
      * @param Int 분
      * @return String a hh:mm 꼴의 문자열
      */
-    fun convertHourMinuteToAmPmFormat(hour: Int, minute: Int): String {
+    fun convertHourMinuteToAmPmFormat(
+        hour: Int,
+        minute: Int,
+    ): String {
         // 날짜는 임의로 설정 (시간 형식만 중요하므로)
         val localDateTime = LocalDateTime.of(2025, 1, 1, hour, minute, 0)
         return localDateTime.format(timeFormatter)
+    }
+
+    /**
+     * yyyy년 MM월 dd일 (E) 꼴의 문자열과 a hh:mm 꼴의 문자열을 받아서
+     * 2025-05-09T03:58:41.360 이런 꼴의 LocalDateTime을 반환하는 함수
+     * @param String yyyy년 MM월 dd일 (E)
+     * @param String a hh:mm
+     * @return String 2025-05-09T03:58:41.360
+     */
+    fun convertStringToLocalDateTime(
+        dateStr: String,
+        timeStr: String,
+    ): LocalDateTime {
+        // 날짜는 임의로 설정 (시간 형식만 중요하므로)
+        val date = LocalDate.parse(dateStr, dateFormatter)
+        val time = LocalTime.parse(timeStr, timeFormatter)
+        val dateTime = LocalDateTime.of(date, time)
+        return dateTime
     }
 
     /** DateDialog의 경우 UTC로 값을 받아서 그렇게 변환하기 위한 함수
@@ -89,5 +109,19 @@ object TimeFormatter {
         val offsetMillis = zoneOffset.totalSeconds * 1000L
 
         return time + offsetMillis
+    }
+
+    /**
+     * 밀리초로 들어온 기간을 00H00M으로 바꾸기 위한 함수
+     * @param Long 기간
+     * @return String 00H00M 꼴
+     */
+    @SuppressLint("DefaultLocale")
+    fun formatDurationToHM(millis: Long): String {
+        val totalMinutes = millis / (1000 * 60)
+        val hours = totalMinutes / 60
+        val minutes = totalMinutes % 60
+
+        return String.format(Locale.getDefault(), "%02dH%02dM", hours, minutes)
     }
 }
