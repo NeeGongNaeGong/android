995b0a355cd964305f5dcb4b39117836d55d2574
전형선 <hsun5163@gmail.com>
2025-05-30 12:59:05 +0900

    refactor(#60): dev 머지 후 변경 detekt 검사 후 통과된 파일
    - 사용되지 않은 param 임시 사용 처리 (groupId, memberId, color)

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/group/record/RecordScreen.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/group/record/RecordScreen.kt
@@ -1,5 +1,6 @@
 package com.ssafy.neegongnaegong.presentation.group.record
 
+import android.util.Log
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -23,17 +25,7 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -50,20 +42,47 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.ssafy.neegongnaegong.domain.model.studygroup.StudyContentInfo
+import com.ssafy.neegongnaegong.domain.model.studygroup.StudyLogByTagInfo
 import com.ssafy.neegongnaegong.presentation.component.TopAppBar
+import com.ssafy.neegongnaegong.presentation.group.record.component.StudyRecordListBySlice
 import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongPreviews
 import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongTheme
+import com.ssafy.neegongnaegong.presentation.util.CustomDateTimeFormatter
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import java.time.LocalDateTime
 
+private const val TAG = "RecordScreen"
 
 @Composable
 fun RecordRoute(
-    groupId: Int,
-    memberId: Int,
+    groupId: Long,
+    memberId: Long,
     popBackStack: () -> Boolean,
     modifier: Modifier = Modifier,
-//    viewModel: RecordViewModel = hiltViewModel()
+    viewModel: RecordViewModel = hiltViewModel(),
 ) {
-//    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    Log.d(TAG, "RecordRoute: $groupId $memberId") // TODO : dekekt 통과 임시 적용
+    val pagingItem = viewModel.studyLogFlow.collectAsLazyPagingItems()
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.effect.collect {
+            when (it) {
+                RecordContract.Effect.NavigateToBackStack -> {
+                    popBackStack()
+                }
+            }
+        }
+    }
+
     Column {
         TopAppBar(
             title = {
@@ -72,110 +91,107 @@ fun RecordRoute(
                     style = NeeGongNaeGongTheme.typography.titleSmall,
                 )
             },
-            onNavigationClick = { popBackStack() }
+            onNavigationClick = { popBackStack() },
         )
 
         RecordContent(
             modifier
-                .weight(1F)
+                .weight(1F),
+            pagingItem,
+            uiState,
         )
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun RecordTopBar(
-    popBackStack: () -> Boolean,
-) {
-    // title을 가운데로 위치시켜주는 CenterAlignedTopAppBar
-    // https://stackoverflow.com/questions/67497414/how-to-align-title-at-layout-center-in-topappbar
-    CenterAlignedTopAppBar(
-        title = {
-            Text(
-                style = NeeGongNaeGongTheme.typography.titleSmall,
-                text = "박준식 이번주 공부내용"
-            )
-        },
-        navigationIcon = {
-            IconButton(onClick = { popBackStack() }) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "뒤로 가기 버튼"
-                )
-            }
-        },
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = NeeGongNaeGongTheme.colorScheme.background,
-            titleContentColor = NeeGongNaeGongTheme.colorScheme.primaryText,
-            navigationIconContentColor = NeeGongNaeGongTheme.colorScheme.primaryText,
-            actionIconContentColor = NeeGongNaeGongTheme.colorScheme.primaryText,
-        )
-    )
 }
 
 @Composable
 fun RecordContent(
     modifier: Modifier = Modifier,
+    pagingItem: LazyPagingItems<StudyContentInfo>,
+    uiState: RecordContract.State,
 ) {
     var chartHeight by remember { mutableStateOf(0.dp) }
 
-    Row(
-        modifier = modifier.padding(horizontal = 15.dp),
-        horizontalArrangement = Arrangement.Center
-    ) {
-        PieChartScreen(
-            modifier = Modifier
-                .height(if (chartHeight > 0.dp) chartHeight else Dp.Unspecified)
-                .aspectRatio(1F)
-        )
-        Spacer(modifier = Modifier.width(10.dp))
-        ChartLegendScreen(
-            modifier = Modifier
-                .wrapContentWidth()
-                .height(if (chartHeight > 0.dp) chartHeight else Dp.Unspecified)
-                .verticalScroll(
-                    rememberScrollState()
-                ),
-            onHeightChange = { newHeight -> chartHeight = newHeight },
-        )
-
-    }
-
-}
-
-@Composable
-fun PieChartScreen(
-    modifier: Modifier = Modifier,
-) {
-// 1) 애니메이션 시작 플래그
-    var startAnimation by remember { mutableStateOf(false) }
-
-    val slices: List<Float> =
-        listOf(0.3F, 0.5F, 0.6F, 0.1F, 0.2F)           // 각 조각의 값 (합은 100이 아니어도 됨)
     val colors: List<Color> =
         listOf(
             NeeGongNaeGongTheme.colorScheme.peach,
             NeeGongNaeGongTheme.colorScheme.lightGreen,
             NeeGongNaeGongTheme.colorScheme.blue,
             NeeGongNaeGongTheme.colorScheme.mintBlue,
-            Color.Magenta
+            Color.Magenta,
         )
 
-    val animationDuration: Int = 1000
+    Column(modifier = modifier) {
+        Row(
+            modifier = Modifier.padding(horizontal = 15.dp),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            PieChartScreen(
+                modifier =
+                    Modifier
+                        .height(if (chartHeight > 0.dp) chartHeight else Dp.Unspecified)
+                        .aspectRatio(1F),
+                uiState.studyLogsByTag,
+                colors,
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            ChartLegendScreen(
+                modifier =
+                    Modifier
+                        .wrapContentWidth()
+                        .height(if (chartHeight > 0.dp) chartHeight else Dp.Unspecified)
+                        .verticalScroll(
+                            rememberScrollState(),
+                        ),
+                studyLogsByTag = uiState.studyLogsByTag,
+                color = colors,
+                onHeightChange = { newHeight -> chartHeight = newHeight },
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        StudyRecordListBySlice(
+            modifier = Modifier.weight(1F),
+            lazyItems = pagingItem,
+            onClick = {},
+        )
+    }
+}
+
+@Composable
+fun PieChartScreen(
+    modifier: Modifier = Modifier,
+    studyLogsByTag: PersistentList<StudyLogByTagInfo>,
+    color: List<Color>,
+) {
+    Log.d(TAG, "PieChartScreen: $color") // TODO : dekekt 통과 임시 적용
+// 1) 애니메이션 시작 플래그
+    var startAnimation by remember { mutableStateOf(false) }
+
+    val slices: List<Float> =
+        studyLogsByTag.map { it.totalSeconds.toFloat() } // 각 조각의 값 (합은 100이 아니어도 됨)
+    val colors: List<Color> =
+        listOf(
+            NeeGongNaeGongTheme.colorScheme.peach,
+            NeeGongNaeGongTheme.colorScheme.lightGreen,
+            NeeGongNaeGongTheme.colorScheme.blue,
+            NeeGongNaeGongTheme.colorScheme.mintBlue,
+            Color.Magenta,
+        )
+
+    val animationDuration = 1000
     // 전체 합계 계산
     val total = remember(slices) { slices.sum() }
-
 
     // 0f → 1f 으로 자동 애니메이트
     val progress by animateFloatAsState(
         targetValue = if (startAnimation) 1f else 0F,
-        animationSpec = tween(
-            durationMillis = animationDuration,
-            easing = LinearEasing
-        )
+        animationSpec =
+            tween(
+                durationMillis = animationDuration,
+                easing = LinearEasing,
+            ),
     )
-
-
 
     LaunchedEffect(Unit) {
         startAnimation = true
@@ -197,18 +213,19 @@ fun PieChartScreen(
             val maxDrawAngle = progress * 360f - 90
 
             // 그릴 양 정하기
-            val drawSweep = when {
-                maxDrawAngle >= startAngle + sweepAngle -> sweepAngle
-                maxDrawAngle <= startAngle -> 0f
-                else -> maxDrawAngle - startAngle
-            }
+            val drawSweep =
+                when {
+                    maxDrawAngle >= startAngle + sweepAngle -> sweepAngle
+                    maxDrawAngle <= startAngle -> 0f
+                    else -> maxDrawAngle - startAngle
+                }
 
             // 조각 그리기
             drawArc(
                 color = colors.getOrElse(index) { Color.Black },
                 startAngle = startAngle,
                 sweepAngle = drawSweep,
-                useCenter = true
+                useCenter = true,
             )
 
             startAngle += drawSweep
@@ -217,101 +234,120 @@ fun PieChartScreen(
 }
 
 @Composable
-fun ChartLegendScreen(onHeightChange: (Dp) -> Unit, modifier: Modifier = Modifier) {
-
-    val legendItems: List<Triple<String, String, Color>> =
-        listOf(
-            Triple("알고리즘", "5H", NeeGongNaeGongTheme.colorScheme.peach),
-            Triple("네트워크", "2H 45M", NeeGongNaeGongTheme.colorScheme.lightGreen),
-            Triple("운영체제", "4H", NeeGongNaeGongTheme.colorScheme.blue),
-            Triple("영어회화", "3H 30M", NeeGongNaeGongTheme.colorScheme.mintBlue),
-            Triple("데이터 베이스", "1H", Color.Magenta),
-        )
-
+fun ChartLegendScreen(
+    onHeightChange: (Dp) -> Unit,
+    modifier: Modifier = Modifier,
+    studyLogsByTag: PersistentList<StudyLogByTagInfo>,
+    color: List<Color>,
+) {
     val density = LocalDensity.current
 
     Column(
         modifier = modifier,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
     ) {
-        legendItems.forEachIndexed { index, item ->
-            Column(
-                modifier = if (index == 0) {
-                    Modifier
-                        .wrapContentSize()//.background(Color.Blue)
-                        .onSizeChanged { size -> // 2. 크기 변경 시 높이 측정
-                            if (size.height > 0) { // 초기 0 값 무시
-                                onHeightChange(with(density) {
-                                    size.height.toDp() * 4 + if (legendItems.size > 1) {
-                                        (-15).dp
-                                    } else {
-                                        0.dp
+        studyLogsByTag.let {
+            it.forEachIndexed { index, item ->
+                Column(
+                    modifier =
+                        if (index == 0) {
+                            Modifier
+                                .wrapContentSize()
+                                .onSizeChanged { size -> // 2. 크기 변경 시 높이 측정
+                                    if (size.height > 0) { // 초기 0 값 무시
+                                        onHeightChange(
+                                            with(density) {
+                                                size.height.toDp() * 4 +
+                                                    if (it.size > 1) {
+                                                        (-15).dp
+                                                    } else {
+                                                        0.dp
+                                                    }
+                                            },
+                                        )
                                     }
-                                })
-                            }
-                        }
-                } else {
-                    Modifier.wrapContentSize()
-                }
-
-            ) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Box(
-                        Modifier
-                            .size(12.dp)
-                            .clip(CircleShape)
-                            .background(item.third)
-                    )
-                    Spacer(Modifier.width(8.dp))
-                    Column {
-                        Text(
-                            modifier = Modifier.basicMarquee(
-                                iterations = Int.MAX_VALUE,
-
-                                ),
-                            style = NeeGongNaeGongTheme.typography.labelMedium,
-                            color = NeeGongNaeGongTheme.colorScheme.chartLegend,
-                            text = item.first, maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
+                                }
+                        } else {
+                            Modifier.wrapContentSize()
+                        },
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Box(
+                            Modifier
+                                .size(12.dp)
+                                .clip(CircleShape)
+                                .background(color[index % 4]),
                         )
+                        Spacer(Modifier.width(8.dp))
+                        Column {
+                            Text(
+                                modifier =
+                                    Modifier.basicMarquee(
+                                        iterations = Int.MAX_VALUE,
+                                    ),
+                                style = NeeGongNaeGongTheme.typography.labelMedium,
+                                // 범례의 컬러를 다크모드일 때 어떤 거로 해야 할지 결정 못함
+                                color = NeeGongNaeGongTheme.colorScheme.chartLegend,
+                                text = item.tagName,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                            )
+                        }
+                    }
+                    Spacer(Modifier.height(5.dp))
+                    Text(
+                        modifier = Modifier.offset(x = (20).dp),
+                        text = CustomDateTimeFormatter.formatDurationToHM(item.totalSeconds),
+                        maxLines = 1,
+                        style = NeeGongNaeGongTheme.typography.bodyLarge,
+                        overflow = TextOverflow.Ellipsis,
+                        fontSize = 16.sp,
+                        color = NeeGongNaeGongTheme.colorScheme.blue,
+                    )
+                    if (index != it.lastIndex) {
+                        Spacer(Modifier.height(15.dp))
                     }
                 }
-                Spacer(Modifier.height(5.dp))
-                Text(
-                    modifier = Modifier.offset(x = (20).dp),
-                    text = item.second,
-                    maxLines = 1,
-                    style = NeeGongNaeGongTheme.typography.bodyLarge,
-                    overflow = TextOverflow.Ellipsis,
-                    fontSize = 16.sp,
-                    color = NeeGongNaeGongTheme.colorScheme.blue,
-                )
-                if (index != legendItems.lastIndex) {
-                    Spacer(Modifier.height(15.dp))
-                }
             }
-
         }
-
-    }
-}
-
-
-@NeeGongNaeGongPreviews
-@Composable
-fun PreviewTopAppBar() {
-    NeeGongNaeGongTheme {
-        RecordTopBar(popBackStack = { true })
     }
 }
 
 @NeeGongNaeGongPreviews
 @Composable
 fun PreviewRecordContent() {
+    val sampleItems =
+        mutableListOf<StudyContentInfo>().apply {
+            repeat(4) {
+                add(
+                    StudyContentInfo(
+                        title = "Kotlin Basics",
+                        learningRecordId = 1,
+                        startAt = LocalDateTime.now(),
+                        endAt = LocalDateTime.now(),
+                        content = "Kotlin Basics",
+                        tags = listOf(),
+                        learningRecordCreatedAt = LocalDateTime.now(),
+                        learningRecordModifiedAt = LocalDateTime.now(),
+                        cursorCreatedAt = LocalDateTime.now(),
+                        cursorId = 0,
+                    ),
+                )
+            }
+        }
+
+    val pagingData = PagingData.from(sampleItems)
+    val lazyItems = MutableStateFlow(pagingData).collectAsLazyPagingItems()
+    val dummyState =
+        RecordContract.State(
+            studyLogsByTag = persistentListOf(),
+        )
     NeeGongNaeGongTheme {
         RecordContent(
             Modifier
                 .fillMaxSize(),
+            pagingItem = lazyItems,
+            dummyState,
         )
     }
 }
@@ -323,6 +359,21 @@ fun PreviewPieChart() {
         PieChartScreen(
             Modifier
                 .fillMaxSize(),
+            studyLogsByTag =
+                persistentListOf(
+                    StudyLogByTagInfo(
+                        tagName = "",
+                        totalSeconds = 1,
+                    ),
+                ),
+            color =
+                listOf(
+                    NeeGongNaeGongTheme.colorScheme.peach,
+                    NeeGongNaeGongTheme.colorScheme.lightGreen,
+                    NeeGongNaeGongTheme.colorScheme.blue,
+                    NeeGongNaeGongTheme.colorScheme.mintBlue,
+                    Color.Magenta,
+                ),
         )
     }
 }
@@ -331,6 +382,24 @@ fun PreviewPieChart() {
 @Composable
 fun PreviewChartLegend() {
     NeeGongNaeGongTheme {
-        ChartLegendScreen({})
+        ChartLegendScreen(
+            modifier = Modifier.fillMaxWidth(),
+            onHeightChange = {},
+            studyLogsByTag =
+                persistentListOf(
+                    StudyLogByTagInfo(
+                        tagName = "",
+                        totalSeconds = 1,
+                    ),
+                ),
+            color =
+                listOf(
+                    NeeGongNaeGongTheme.colorScheme.peach,
+                    NeeGongNaeGongTheme.colorScheme.lightGreen,
+                    NeeGongNaeGongTheme.colorScheme.blue,
+                    NeeGongNaeGongTheme.colorScheme.mintBlue,
+                    Color.Magenta,
+                ),
+        )
     }
 }
