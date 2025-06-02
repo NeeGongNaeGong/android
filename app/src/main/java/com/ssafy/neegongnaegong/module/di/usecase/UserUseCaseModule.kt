498bcee1c4361296e73c43bc57bd3e5b8de50184
Aloe-droid <csjune99@naver.com>
2025-06-02 09:32:16 +0900

    feat(#89) getUser UseCase 싱글톤 제거

--- app/src/main/java/com/ssafy/neegongnaegong/module/di/usecase/UserUseCaseModule.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/module/di/usecase/UserUseCaseModule.kt
@@ -1,7 +1,6 @@
 package com.ssafy.neegongnaegong.module.di.usecase
 
 import com.ssafy.neegongnaegong.domain.repository.UserRepository
-import com.ssafy.neegongnaegong.domain.usecase.user.GetCurrentUserUseCase
 import com.ssafy.neegongnaegong.domain.usecase.user.GetUserUseCase
 import com.ssafy.neegongnaegong.domain.usecase.user.UpdateNicknameUseCase
 import com.ssafy.neegongnaegong.domain.usecase.user.UpdateProfileImageUseCase
@@ -15,9 +14,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object UserUseCaseModule {
-    @Provides
-    @Singleton
-    fun provideGetCurrentUserUseCase(userRepository: UserRepository): GetCurrentUserUseCase = GetCurrentUserUseCase(userRepository)
 
     @Provides
     @Singleton
