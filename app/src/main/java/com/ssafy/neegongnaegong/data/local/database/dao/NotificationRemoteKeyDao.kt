e79520cb3a8e0252343a79a896ea910dd0572155
Aloe-droid <csjune99@naver.com>
2025-06-02 15:56:40 +0900

    chore(#91) Notification Local DAO 정의

---
+++ app/src/main/java/com/ssafy/neegongnaegong/data/local/database/dao/NotificationRemoteKeyDao.kt
@@ -0,0 +1,19 @@
+package com.ssafy.neegongnaegong.data.local.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ssafy.neegongnaegong.data.local.database.entity.NotificationRemoteKeyEntity
+
+@Dao
+interface NotificationRemoteKeyDao {
+    @Query("SELECT * FROM notification_remote_keys WHERE id = :id")
+    suspend fun getRemoteKey(id: Long): NotificationRemoteKeyEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(keys: List<NotificationRemoteKeyEntity>)
+
+    @Query("DELETE FROM notification_remote_keys")
+    suspend fun clearAll()
+}
