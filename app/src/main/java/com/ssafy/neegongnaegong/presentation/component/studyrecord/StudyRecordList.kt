e50a444dfd37237e1315711667e947aa74cd7bb3
김민조 <upsk1@naver.com>
2025-06-02 18:25:59 +0900

    feat(#73): 기본은 로딩 페이지 추가

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/component/studyrecord/StudyRecordList.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/component/studyrecord/StudyRecordList.kt
@@ -1,14 +1,19 @@
 package com.ssafy.neegongnaegong.presentation.component.studyrecord
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.overscroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -24,6 +29,7 @@ import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongTheme
 import com.ssafy.neegongnaegong.presentation.util.toDateString
 import kotlinx.coroutines.flow.distinctUntilChanged
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun StudyRecordList(
     modifier: Modifier = Modifier,
@@ -46,6 +52,7 @@ fun StudyRecordList(
         }
     } else {
         val listState = rememberLazyListState()
+        val overscrollEffect = ScrollableDefaults.overscrollEffect()
 
         val groupedRecords =
             learningRecords
@@ -61,13 +68,16 @@ fun StudyRecordList(
                 .collect { lastVisibleItemIndex ->
                     val totalItemCount = listState.layoutInfo.totalItemsCount
                     if (hasNext && lastVisibleItemIndex == totalItemCount - 1) {
-                        onLoadMore()
+//                        onLoadMore()
                     }
                 }
         }
 
         LazyColumn(
-            modifier = modifier,
+            modifier =
+                modifier
+                    .fillMaxSize()
+                    .overscroll(overscrollEffect),
             state = listState,
             verticalArrangement = Arrangement.spacedBy(12.dp),
         ) {
@@ -84,8 +94,23 @@ fun StudyRecordList(
                     StudyRecordItem(record = record, onClick = onClick)
                 }
             }
-            item {
-                Spacer(modifier = Modifier.height(0.dp))
+
+            if (hasNext) {
+                item {
+                    Box(
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .height(72.dp),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.offset(y = (-4).dp),
+                            color = NeeGongNaeGongTheme.colorScheme.gray4,
+                            strokeWidth = 2.dp,
+                        )
+                    }
+                }
             }
         }
     }
