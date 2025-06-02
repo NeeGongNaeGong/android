bfa65a86d4fc63581d03cd455e56528b40aec0f6
전형선 <hsun5163@gmail.com>
2025-06-02 16:10:08 +0900

    feat(#60): 스터디 삭제 Dialog 구현
    - 스터디 삭제 요청을 확인하는 Dialog 구현

---
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/group/component/drawer/DeleteStudiesDialog.kt
@@ -0,0 +1,122 @@
+package com.ssafy.neegongnaegong.presentation.group.component.drawer
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongPreviews
+import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongTheme
+
+@Composable
+fun DeleteStudiesDialog(
+    modifier: Modifier = Modifier,
+    onCancel: () -> Unit,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+) {
+    Dialog(onDismissRequest = { onCancel.invoke() }) {
+        Box(
+            modifier =
+                modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = NeeGongNaeGongTheme.colorScheme.background,
+                        shape = RoundedCornerShape(12.dp),
+                    ),
+        ) {
+            Column(
+                modifier = Modifier.padding(top = 20.dp, start = 10.dp, end = 10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "스터디 삭제",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = NeeGongNaeGongTheme.colorScheme.primaryText,
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                Text(
+                    text = "스터디를 삭제 할까요?",
+                    fontSize = 14.sp,
+                    color = Color.Gray,
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                HorizontalDivider(thickness = 0.4.dp, color = Color.Gray)
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    TextButton(
+                        onClick = onDismiss,
+                        modifier = Modifier.weight(1f),
+                    ) {
+                        Text(
+                            text = "취소",
+                            style = NeeGongNaeGongTheme.typography.labelLarge,
+                            fontWeight = FontWeight.Bold,
+                            color = NeeGongNaeGongTheme.colorScheme.secondaryText,
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    VerticalDivider(
+                        thickness = 0.4.dp,
+                        color = Color.Gray,
+                        modifier = Modifier.height(50.dp),
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    TextButton(
+                        onClick = onConfirm,
+                        modifier = Modifier.weight(1f),
+                    ) {
+                        Text(
+                            text = "삭제",
+                            style = NeeGongNaeGongTheme.typography.labelLarge,
+                            fontWeight = FontWeight.Bold,
+                            color = NeeGongNaeGongTheme.colorScheme.peach,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@NeeGongNaeGongPreviews
+@Composable
+private fun PreviewDeleteStudiesDialog() {
+    NeeGongNaeGongTheme {
+        DeleteStudiesDialog(
+            onCancel = {},
+            onDismiss = {},
+            onConfirm = {},
+        )
+    }
+}
