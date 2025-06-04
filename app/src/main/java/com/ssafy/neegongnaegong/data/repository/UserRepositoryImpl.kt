276e885142feb4d550b013e1df42aefddbca44b5
Aloe-droid <csjune99@naver.com>
2025-06-04 15:01:54 +0900

    feat(#90) User 검색 Repository 구현

--- app/src/main/java/com/ssafy/neegongnaegong/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/repository/UserRepositoryImpl.kt
@@ -1,17 +1,23 @@
 package com.ssafy.neegongnaegong.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.ssafy.neegongnaegong.data.datasource.local.LocalFcmDataSource
 import com.ssafy.neegongnaegong.data.datasource.local.LocalUserDataSource
 import com.ssafy.neegongnaegong.data.datasource.network.NetworkUserDataSource
 import com.ssafy.neegongnaegong.data.mapper.user.UserMapper.toDomain
 import com.ssafy.neegongnaegong.data.model.user.request.UpdateFcmTokenRequest
 import com.ssafy.neegongnaegong.data.model.user.request.UpdateUserRequest
+import com.ssafy.neegongnaegong.data.model.user.response.UserResponse
+import com.ssafy.neegongnaegong.data.paging.UserPagingSource
 import com.ssafy.neegongnaegong.domain.model.User
 import com.ssafy.neegongnaegong.domain.repository.UserRepository
 import com.ssafy.neegongnaegong.module.di.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -20,7 +26,8 @@ class UserRepositoryImpl @Inject constructor(
     private val localUserDataSource: LocalUserDataSource,
     private val localFcmDataSource: LocalFcmDataSource,
     private val networkUserDataSource: NetworkUserDataSource,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    private val pagingSourceFactory: UserPagingSource.Factory,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : UserRepository {
     override suspend fun getUser(): Flow<User> = withContext(ioDispatcher) {
         localUserDataSource.getUser()
@@ -56,5 +63,17 @@ class UserRepositoryImpl @Inject constructor(
 
     override suspend fun checkUpdateFcmTokenState(): Boolean = withContext(ioDispatcher) {
         localFcmDataSource.getUpdateFcmTokenState()
+    }
+
+    override fun searchUser(userName: String): Flow<PagingData<User>> = Pager(
+        config = PagingConfig(pageSize = USER_PAGING_SIZE),
+        pagingSourceFactory = { pagingSourceFactory.create(userName = userName) }
+    ).flow.flowOn(context = ioDispatcher)
+        .map { pagingData: PagingData<UserResponse> ->
+            pagingData.toDomain()
+        }
+
+    companion object {
+        private const val USER_PAGING_SIZE = 30
     }
 }
