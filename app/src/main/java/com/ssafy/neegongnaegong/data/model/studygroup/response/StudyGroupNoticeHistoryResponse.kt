be854e8bedebedb3a823640fcbbc96360bcdfe6b
Na2te <111sym@naver.com>
2025-06-04 14:12:44 +0900

    feat(#86): StudyGroupNoticeHistoryResponse 생성 - 공지사항 리스트 API 호출 결과를 받는 DataLayer 모델 생성

---
+++ app/src/main/java/com/ssafy/neegongnaegong/data/model/studygroup/response/StudyGroupNoticeHistoryResponse.kt
@@ -0,0 +1,20 @@
+package com.ssafy.neegongnaegong.data.model.studygroup.response
+
+import java.time.LocalDateTime
+
+data class StudyGroupNoticeHistoryResponse(
+    val id: Long,
+    val cursorId: Long,
+    val title: String,
+    val contentPreview: String,
+    val createdAt: LocalDateTime,
+    val modifiedAt: LocalDateTime,
+    val writer: Writer,
+)
+
+data class Writer(
+    val userId: Long,
+    val name: String,
+    val profileImg: String,
+    val groupRole: String,
+)
