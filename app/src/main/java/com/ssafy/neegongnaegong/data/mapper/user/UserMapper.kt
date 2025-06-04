4288c3e73f6e386115d24ac5d58c03c303107161
Aloe-droid <csjune99@naver.com>
2025-06-04 15:01:11 +0900

    feat(#90) UserResponse Domain Mapper 정의

--- app/src/main/java/com/ssafy/neegongnaegong/data/mapper/user/UserMapper.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/mapper/user/UserMapper.kt
@@ -1,6 +1,9 @@
 package com.ssafy.neegongnaegong.data.mapper.user
 
+import androidx.paging.PagingData
+import androidx.paging.map
 import com.ssafy.neegongnaegong.data.model.user.response.UserDetailResponse
+import com.ssafy.neegongnaegong.data.model.user.response.UserResponse
 import com.ssafy.neegongnaegong.domain.model.User
 
 internal object UserMapper {
@@ -10,4 +13,13 @@ internal object UserMapper {
         nickname = nickname,
         profileImg = profileImg
     )
+
+    fun UserResponse.toDomain() = User(
+        id = id,
+        email = email,
+        nickname = nickName,
+        profileImg = profileImg
+    )
+
+    fun PagingData<UserResponse>.toDomain(): PagingData<User> = map { it.toDomain() }
 }
