09d1d1c199bd53611f3b6290fc9c79fc69a33ef4
전형선 <hsun5163@gmail.com>
2025-06-05 15:25:22 +0900

    refactor(#94): BottomNavigation 학습 타이머 fab button 디자인 변경
    - icon vector 변경 (compact하게)
    - size 조절

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/navigation/BottomNavigationBar.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -152,13 +151,12 @@ fun BottomNavigationBar(
                 modifier =
                     Modifier
                         .size(56.dp)
-                        .padding(4.dp),
+                        .padding(8.dp),
             )
         }
     }
 }
 
-@Preview(showBackground = true, heightDp = 70)
 @NeeGongNaeGongPreviews
 @Composable
 fun PreviewBottomNavigationBar() {
