e72b48d5211d5dcba74fbedbc1cdc2dd67ece2a1
Aloe-droid <csjune99@naver.com>
2025-06-07 10:46:49 +0900

    feat(#100) 알람 단건 조회, 읽음 처리 DataSource 구현

--- app/src/main/java/com/ssafy/neegongnaegong/data/datasource/network/NetworkNotificationDataSourceImpl.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/datasource/network/NetworkNotificationDataSourceImpl.kt
@@ -1,9 +1,12 @@
 package com.ssafy.neegongnaegong.data.datasource.network
 
 import com.ssafy.neegongnaegong.data.model.apiFlow
+import com.ssafy.neegongnaegong.data.model.notification.GetNotificationResponse
 import com.ssafy.neegongnaegong.data.model.notification.NotificationPage
 import com.ssafy.neegongnaegong.data.remote.NotificationApi
+import com.ssafy.neegongnaegong.domain.exception.ApiException
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import javax.inject.Inject
 
 class NetworkNotificationDataSourceImpl @Inject constructor(
@@ -17,11 +20,22 @@ class NetworkNotificationDataSourceImpl @Inject constructor(
         notificationApi.getNotifications(cursorId = cursorId, size = size)
     }
 
+    override fun getNotification(notificationId: Long): Flow<GetNotificationResponse> = apiFlow {
+        notificationApi.getNotification(notificationId = notificationId)
+    }
+
     override fun deleteNotification(notificationId: Long): Flow<Unit> = apiFlow {
         notificationApi.deleteNotification(notificationId = notificationId)
     }
 
     override fun deleteAllNotifications(): Flow<Unit> = apiFlow {
         notificationApi.deleteAllNotifications()
+    }
+
+    override fun readNotification(notificationId: Long): Flow<Unit> = apiFlow {
+        notificationApi.readNotification(notificationId = notificationId)
+    }.catch { throwable: Throwable ->
+        if (throwable is ApiException.ConflictException) emit(value = Unit)
+        else throw throwable
     }
 }
