ad40ccb1d6192ded8308ed678ddec838e88b0837
Aloe-droid <csjune99@naver.com>
2025-06-07 10:51:27 +0900

    feat(#100) 그룹 초대 수락/거절 Repository 구현

--- app/src/main/java/com/ssafy/neegongnaegong/domain/repository/StudyGroupRepository.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/domain/repository/StudyGroupRepository.kt
@@ -4,11 +4,22 @@ import androidx.paging.PagingData
 import com.ssafy.neegongnaegong.domain.model.studygroup.StudyContentInfo
 import com.ssafy.neegongnaegong.domain.model.studygroup.StudyLogByTagInfo
 import com.ssafy.neegongnaegong.domain.model.studygroup.StudyMemberInfo
-import kotlinx.collections.immutable.PersistentList
 import kotlinx.coroutines.flow.Flow
 
 interface StudyGroupRepository {
     fun getMemberStudyLogsByTag(request: StudyMemberInfo): Flow<List<StudyLogByTagInfo>>
 
     fun getMemberStudyContents(request: StudyMemberInfo): Flow<PagingData<StudyContentInfo>>
+
+    fun approveStudyGroupJoin(
+        studyGroupId: Long,
+        userId: Long,
+        notificationId: Long?
+    ): Flow<Unit>
+
+    fun rejectStudyGroupJoin(
+        studyGroupId: Long,
+        userId: Long,
+        notificationId: Long?
+    ): Flow<Unit>
 }
