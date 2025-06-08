e7fdf09c7c4b4fcb6c289f8e404f30aa751714ad
김민조 <upsk1@naver.com>
2025-06-08 19:41:09 +0900

    feat(#104): ViewModel 생성

---
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/group/user/search/UserSearchViewModel.kt
@@ -0,0 +1,31 @@
+package com.ssafy.neegongnaegong.presentation.group.user.search
+
+import com.ssafy.neegongnaegong.domain.usecase.user.SearchUserUseCase
+import com.ssafy.neegongnaegong.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class UserSearchViewModel
+    @Inject
+    constructor(
+        private val searchUserUseCase: SearchUserUseCase,
+    ) : BaseViewModel<UserSearchContract.Event, UserSearchContract.State, UserSearchContract.Effect>() {
+        override fun createInitialState(): UserSearchContract.State = UserSearchContract.State()
+
+        override fun handleEvent(event: UserSearchContract.Event) {
+            when (event) {
+                is UserSearchContract.Event.OnTypingSearch -> {
+                    handleOnTypingSearch(event.keyword)
+                }
+
+                is UserSearchContract.Event.OnClickSearch -> {
+                    handleOnClickSearch(event.keyword)
+                }
+            }
+        }
+
+        private fun handleOnTypingSearch(searchWord: String) {}
+
+        private fun handleOnClickSearch(searchWord: String) {}
+    }
