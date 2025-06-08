39f2e8cdb0651c37fa7e526a5dec93253ae7dc49
전형선 <hsun5163@gmail.com>
2025-06-09 07:16:50 +0900

    refactor(#60): StudiesMember domain model 역할 타입 수정
    - String -> enum class

--- app/src/main/java/com/ssafy/neegongnaegong/data/mapper/studies/StudiesMemberMapper.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/mapper/studies/StudiesMemberMapper.kt
@@ -2,6 +2,7 @@ package com.ssafy.neegongnaegong.data.mapper.studies
 
 import com.ssafy.neegongnaegong.data.model.studies.response.GetMemberResponse
 import com.ssafy.neegongnaegong.domain.model.studies.StudiesMember
+import com.ssafy.neegongnaegong.presentation.group.role.component.StudiesMemberRole
 
 internal object StudiesMemberMapper {
     fun GetMemberResponse.toDomain() =
@@ -9,7 +10,7 @@ internal object StudiesMemberMapper {
             userId = userId,
             name = name,
             profileImg = profileImg,
-            groupRole = groupRole,
+            groupRole = StudiesMemberRole.valueOf(groupRole),
         )
 
     fun List<GetMemberResponse>.toDomain() = map { it.toDomain() }
