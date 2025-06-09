d299d0ab40becfc8d24e897f7b5532a0d3df6257
Na2te <111sym@naver.com>
2025-06-09 12:52:26 +0900

    fix(#86): 투표 생성 인자 변경 - multiple 등 속성들에 prefix로 is를 붙이는 걸로 수정

--- app/src/main/java/com/ssafy/neegongnaegong/data/mapper/vote/VoteMapper.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/mapper/vote/VoteMapper.kt
@@ -4,16 +4,16 @@ import com.ssafy.neegongnaegong.data.model.studies.request.CreateVoteRequest
 import com.ssafy.neegongnaegong.domain.model.studies.VoteInfo
 
 internal object VoteMapper {
-
-    fun VoteInfo.toCreateRequest() = CreateVoteRequest(
-        title = title,
-        startTime = startTime,
-        endTime = endTime,
-        state = state,
-        items = items,
-        multiple = multiple,
-        secret = secret,
-        notify = notify,
-        choose = choose
-    )
+    fun VoteInfo.toCreateRequest() =
+        CreateVoteRequest(
+            title = title,
+            startTime = startTime,
+            endTime = endTime,
+            state = state,
+            items = items,
+            isMultiple = multiple,
+            isSecret = secret,
+            isNotify = notify,
+            isChoose = choose,
+        )
 }
