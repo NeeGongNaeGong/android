853ff649bdd0f411dab1b3a10a9b3e57dfd19b11
Na2te <111sym@naver.com>
2025-06-09 12:49:41 +0900

    feat(#86): StudyGroupVoteDetailInfoMapper 수정 - 투표 항목별 참여한 사람 데이터를 Domain 모델로 변환하는 Mapper 구현

--- app/src/main/java/com/ssafy/neegongnaegong/data/mapper/studygroup/StudyGroupVoteDetailInfoMapper.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/mapper/studygroup/StudyGroupVoteDetailInfoMapper.kt
@@ -1,26 +1,47 @@
 package com.ssafy.neegongnaegong.data.mapper.studygroup
 
+import com.ssafy.neegongnaegong.data.mapper.studygroup.StudyGroupVoteDetailInfoMapper.toDomain
 import com.ssafy.neegongnaegong.data.model.studygroup.response.StudyGroupVoteDetailResponse
 import com.ssafy.neegongnaegong.data.model.studygroup.response.StudyGroupVoteStatus
 import com.ssafy.neegongnaegong.domain.model.studygroup.StudyGroupVoteDetailInfo
 import com.ssafy.neegongnaegong.domain.model.studygroup.StudyGroupVoteStatusInfo
 
 internal object StudyGroupVoteDetailInfoMapper {
+    fun StudyGroupVoteStatus.VotedMemberStatus.toDomain() =
+        StudyGroupVoteStatusInfo.VotedMemberInfo(
+            id = id,
+            username = username,
+            profileImg = profileImg,
+        )
+
+    private fun List<StudyGroupVoteStatus.VotedMemberStatus>.toVoteMemberStatusDomain() = map { it.toDomain() }
+
     fun StudyGroupVoteStatus.toDomain() =
         StudyGroupVoteStatusInfo(
+            voteItemId = voteItemId,
             voteItemName = voteItemName,
             voteItemValue = voteItemValue,
-            voteMembers = voteMembers,
+            votedMembers = votedMembers.toVoteMemberStatusDomain(),
         )
 
     fun List<StudyGroupVoteStatus>.toDomain() = map { it.toDomain() }
 
+    fun StudyGroupVoteDetailResponse.VoteValue.toDomain() =
+        StudyGroupVoteDetailInfo.VoteValue(
+            voteItemId = voteItemId,
+            voteItemName = voteItemName,
+        )
+
+    private fun List<StudyGroupVoteDetailResponse.VoteValue>.toVoteValueDomain() = map { it.toDomain() }
+
     fun StudyGroupVoteDetailResponse.toDomain() =
         StudyGroupVoteDetailInfo(
             userName = userName,
+            userProfileImg = userProfileImg,
             progressTime = progressTime,
             voteTitle = voteTitle,
             voteOptions = voteOptions,
             voteItems = voteItems.toDomain(),
+            voteValues = voteValues.toVoteValueDomain(),
         )
 }
