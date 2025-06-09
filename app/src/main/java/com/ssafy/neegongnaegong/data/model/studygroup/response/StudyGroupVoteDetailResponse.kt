eafbe96d0792453fb27040c7fe14b59bd3a633cd
Na2te <111sym@naver.com>
2025-06-09 12:55:38 +0900

    fix(#86): 투표 상세정보 response 수정 - 투표 생성자의 이미지 url등을 수신받도록 수정

--- app/src/main/java/com/ssafy/neegongnaegong/data/model/studygroup/response/StudyGroupVoteDetailResponse.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/model/studygroup/response/StudyGroupVoteDetailResponse.kt
@@ -2,8 +2,12 @@ package com.ssafy.neegongnaegong.data.model.studygroup.response
 
 data class StudyGroupVoteDetailResponse(
     val userName: String,
+    val userProfileImg: String,
     val progressTime: String,
     val voteTitle: String,
     val voteOptions: List<String>,
     val voteItems: List<StudyGroupVoteStatus>,
-)
+    val voteValues: List<VoteValue>,
+) {
+    data class VoteValue(val voteItemId: Long, val voteItemName: String)
+}
--- app/src/main/java/com/ssafy/neegongnaegong/domain/model/studygroup/StudyGroupVoteDetailInfo.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/domain/model/studygroup/StudyGroupVoteDetailInfo.kt
@@ -2,8 +2,12 @@ package com.ssafy.neegongnaegong.domain.model.studygroup
 
 data class StudyGroupVoteDetailInfo(
     val userName: String,
+    val userProfileImg: String,
     val progressTime: String,
     val voteTitle: String,
     val voteOptions: List<String>,
     val voteItems: List<StudyGroupVoteStatusInfo>,
-)
+    val voteValues: List<VoteValue>,
+) {
+    data class VoteValue(val voteItemId: Long, val voteItemName: String)
+}
