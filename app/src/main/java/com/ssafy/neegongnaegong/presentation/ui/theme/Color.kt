1f50d31d99b2f707f2f0e0d8289538b289958a29
Na2te <111sym@naver.com>
2025-06-09 13:23:34 +0900

    feat(#86): Color 수정 - 기존 Color 파일의 neegongnaegongcolors로 되어 있던 것이 detekt에 막혀 Color로 수정 - 투표시 버튼 활성화 색깔인 yellow 추가

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.ssafy.neegongnaegong.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-data class NeeGongNaeGongColors(
+data class Color(
     val primaryText: Color,
     val secondaryText: Color,
     val blue: Color,
@@ -15,37 +15,42 @@ data class NeeGongNaeGongColors(
     val gray2: Color,
     val gray1: Color,
     val background: Color,
-    val chartLegend: Color
+    val chartLegend: Color,
+    val yellow: Color,
 )
 
-val LightColorScheme = NeeGongNaeGongColors(
-    blue = Color(0xFF82ACCF),
-    mintBlue = Color(0xFFB5E7E5),
-    mint = Color(0xFFA5F7CB),
-    lightGreen = Color(0xFFD2FC90),
-    peach = Color(0xFFFFBBBB),
-    primaryText = Color(0xFF222227),
-    gray4 = Color(0xFF949494),
-    gray3 = Color(0xFFD9D9D9),
-    gray2 = Color(0xFFF4F4F4),
-    gray1 = Color(0xFFF9F9F9),
-    secondaryText = Color(0xFF949494),
-    background = Color(0xFFFAFAFA),
-    chartLegend = Color(0x612E2626)
-)
+val LightColorScheme =
+    Color(
+        blue = Color(0xFF82ACCF),
+        mintBlue = Color(0xFFB5E7E5),
+        mint = Color(0xFFA5F7CB),
+        lightGreen = Color(0xFFD2FC90),
+        peach = Color(0xFFFFBBBB),
+        primaryText = Color(0xFF222227),
+        gray4 = Color(0xFF949494),
+        gray3 = Color(0xFFD9D9D9),
+        gray2 = Color(0xFFF4F4F4),
+        gray1 = Color(0xFFF9F9F9),
+        secondaryText = Color(0xFF949494),
+        background = Color(0xFFFAFAFA),
+        chartLegend = Color(0x612E2626),
+        yellow = Color(0xFFF9F871),
+    )
 
-val DarkColorScheme = NeeGongNaeGongColors(
-    blue = Color(0xFF4A708E),
-    mintBlue = Color(0xFF7FCBCB),
-    mint = Color(0xFF6FCFA6),
-    lightGreen = Color(0xFF9BBB5C),
-    peach = Color(0xFFCC8888),
-    primaryText = Color(0xFFEFEFEF),
-    gray4 = Color(0xFF666666),
-    gray3 = Color(0xFF555555),
-    gray2 = Color(0xFF333333),
-    gray1 = Color(0xFF222222),
-    secondaryText = Color(0xFFAAAAAA),
-    background = Color(0xFF121212),
-    chartLegend = Color(0x612E2626)
-)
+val DarkColorScheme =
+    Color(
+        blue = Color(0xFF4A708E),
+        mintBlue = Color(0xFF7FCBCB),
+        mint = Color(0xFF6FCFA6),
+        lightGreen = Color(0xFF9BBB5C),
+        peach = Color(0xFFCC8888),
+        primaryText = Color(0xFFEFEFEF),
+        gray4 = Color(0xFF666666),
+        gray3 = Color(0xFF555555),
+        gray2 = Color(0xFF333333),
+        gray1 = Color(0xFF222222),
+        secondaryText = Color(0xFFAAAAAA),
+        background = Color(0xFF121212),
+        chartLegend = Color(0x612E2626),
+        yellow = Color(0xFFF9F871),
+    )
