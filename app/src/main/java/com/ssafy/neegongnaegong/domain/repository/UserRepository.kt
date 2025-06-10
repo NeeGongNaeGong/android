f162c0214bbaef17a799a8c7ded62eb4522e098f
Aloe-droid <csjune99@naver.com>
2025-06-10 16:03:10 +0900

    feat(#114) 프로필 이미지 경고성 메시지 정보 저장을 위한 Repository 정의

--- app/src/main/java/com/ssafy/neegongnaegong/domain/repository/UserRepository.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/domain/repository/UserRepository.kt
@@ -6,15 +6,28 @@ import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
     fun getUser(): Flow<User>
+
     fun validateNickname(nickname: String): Flow<Boolean>
+
     fun updateNickname(nickname: String): Flow<Unit>
 
     suspend fun getUser(id: Long): Flow<User>
+
     suspend fun updateProfileImage(profileImage: String): Flow<Unit>
+
     suspend fun updateFcmToken(fcmToken: String? = null)
+
     suspend fun checkUpdateFcmTokenState(): Boolean
+
     fun searchUser(userName: String): Flow<PagingData<User>>
+
     fun findUnReadNotification(): Flow<Boolean>
+
     fun logout(): Flow<Unit>
+
     fun withdraw(): Flow<Unit>
+
+    fun saveProfileImageWarningAcceptedAt(time: Long)
+
+    fun getProfileImageWarningAcceptedAt(): Flow<Long>
 }
