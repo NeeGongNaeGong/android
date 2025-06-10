de8cfc43658b773d495cf480b675e511d9219041
Na2te <111sym@naver.com>
2025-06-10 20:25:04 +0900

    fix(#86): userProfileImage Nullable 변경 - StudyGroupNoticeDetailInfo의 userProfileImage 속성 NonNullable하게 설정 - StudyGroupNoticeDetailResponse의 userProfileImage 속성 NonNullable하게 설정

--- app/src/main/java/com/ssafy/neegongnaegong/data/model/studygroup/response/StudyGroupNoticeDetailResponse.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/data/model/studygroup/response/StudyGroupNoticeDetailResponse.kt
@@ -8,6 +8,6 @@ data class StudyGroupNoticeDetailResponse(
     val content: String,
     val createdAt: LocalDateTime,
     val modifiedAt: LocalDateTime,
-    val userProfileImage: String?,
+    val userProfileImage: String,
     val writer: Writer,
 )
