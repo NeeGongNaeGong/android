81ef4ff9b22df201a591a6d5e7fa86ce9a529a26
Na2te <111sym@naver.com>
2025-06-10 22:48:34 +0900

    fix(#86): 불필요한 Serilizable 어노테이션 제거

--- app/src/main/java/com/ssafy/neegongnaegong/domain/model/studygroup/StudyGroupVoteStatusInfo.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/domain/model/studygroup/StudyGroupVoteStatusInfo.kt
@@ -1,14 +1,10 @@
 package com.ssafy.neegongnaegong.domain.model.studygroup
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class StudyGroupVoteStatusInfo(
     val voteItemId: Long,
     val voteItemName: String,
     val voteItemValue: Long,
     val votedMembers: List<VotedMemberInfo>,
 ) {
-    @Serializable
     data class VotedMemberInfo(val id: Long, val username: String, val profileImg: String)
 }
