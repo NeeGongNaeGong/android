6653279a3020743ba444e5760c8a0c18d5d5b7f1
Na2te <111sym@naver.com>
2025-06-12 19:13:38 +0900

    fix(#86): 투표 항목 투표 인원 기능 관련 버그 수정 - 튕기던 버그 수정 => Json 처리 관련 에러 - 추후 UiModel 등 별도로 presentation 영역으로 빼서 처리할 예정

--- app/src/main/java/com/ssafy/neegongnaegong/domain/model/studygroup/StudyGroupVoteStatusInfo.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/domain/model/studygroup/StudyGroupVoteStatusInfo.kt
@@ -1,10 +1,13 @@
 package com.ssafy.neegongnaegong.domain.model.studygroup
 
+import kotlinx.serialization.Serializable
+
 data class StudyGroupVoteStatusInfo(
     val voteItemId: Long,
     val voteItemName: String,
     val voteItemValue: Long,
     val votedMembers: List<VotedMemberInfo>,
 ) {
+    @Serializable
     data class VotedMemberInfo(val id: Long, val username: String, val profileImg: String)
 }
