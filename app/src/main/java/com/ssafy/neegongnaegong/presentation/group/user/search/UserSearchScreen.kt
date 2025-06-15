ea1e6bbb6527186a742c178a04b0d0b352aed854
김민조 <upsk1@naver.com>
2025-06-15 16:08:40 +0900

    feat(#122): 뒤로가기 버튼 추가

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/group/user/search/UserSearchScreen.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/group/user/search/UserSearchScreen.kt
@@ -2,12 +2,19 @@ package com.ssafy.neegongnaegong.presentation.group.user.search
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -117,14 +124,35 @@ fun UserSearchScreen(
     ) {
         Spacer(modifier = Modifier.height(12.dp))
 
-        UserSearchTextField(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 4.dp),
-            content = query,
-            onContentChanged = onTypeSearch,
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Spacer(modifier = Modifier.width(4.dp))
+
+            IconButton(
+                modifier =
+                    Modifier
+                        .padding(start = 6.dp)
+                        .size(24.dp),
+                onClick = popBackStack,
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "뒤로가기",
+                    tint = NeeGongNaeGongTheme.colorScheme.primaryText,
+                )
+            }
+
+            UserSearchTextField(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(end = 4.dp),
+                content = query,
+                onContentChanged = onTypeSearch,
+            )
+        }
 
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
