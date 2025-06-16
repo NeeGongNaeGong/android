dcc27c090c495e9939a5dbb0ce2b1e8c3388c373
김민조 <upsk1@naver.com>
2025-06-16 18:42:02 +0900

    feat(#122): 다크모드 ,라이트모드 분기별처리

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/timer/TimerActivity.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/timer/TimerActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -62,11 +63,28 @@ class TimerActivity : ComponentActivity() {
         setContent {
             NeeGongNaeGongTheme {
                 val backgroundColor = NeeGongNaeGongTheme.colorScheme.background.toArgb()
+                val isDarkTheme = isSystemInDarkTheme()
 
                 SideEffect {
                     enableEdgeToEdge(
-                        statusBarStyle = SystemBarStyle.dark(scrim = backgroundColor),
-                        navigationBarStyle = SystemBarStyle.dark(scrim = backgroundColor),
+                        statusBarStyle =
+                            if (isDarkTheme) {
+                                SystemBarStyle.dark(scrim = backgroundColor)
+                            } else {
+                                SystemBarStyle.light(
+                                    scrim = backgroundColor,
+                                    darkScrim = backgroundColor,
+                                )
+                            },
+                        navigationBarStyle =
+                            if (isDarkTheme) {
+                                SystemBarStyle.dark(scrim = backgroundColor)
+                            } else {
+                                SystemBarStyle.light(
+                                    scrim = backgroundColor,
+                                    darkScrim = backgroundColor,
+                                )
+                            },
                     )
                 }
 
