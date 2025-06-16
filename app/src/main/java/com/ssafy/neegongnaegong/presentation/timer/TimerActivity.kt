760b8470a44aea196f6fcd2e6adebfa5d8f97d26
김민조 <upsk1@naver.com>
2025-06-16 16:09:59 +0900

    feat(#122): 상태바, 바텀바 색상 지정

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/timer/TimerActivity.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/timer/TimerActivity.kt
@@ -6,13 +6,17 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ssafy.neegongnaegong.presentation.component.snackbar.NeeGongNaeGongSnackbarHost
 import com.ssafy.neegongnaegong.presentation.timer.learning.LearningRecordWriteRoute
@@ -57,9 +61,21 @@ class TimerActivity : ComponentActivity() {
 
         setContent {
             NeeGongNaeGongTheme {
+                val backgroundColor = NeeGongNaeGongTheme.colorScheme.background.toArgb()
+
+                SideEffect {
+                    enableEdgeToEdge(
+                        statusBarStyle = SystemBarStyle.dark(scrim = backgroundColor),
+                        navigationBarStyle = SystemBarStyle.dark(scrim = backgroundColor),
+                    )
+                }
+
                 Scaffold(snackbarHost = { NeeGongNaeGongSnackbarHost() }) { innerPadding ->
+
                     LearningRoute(
-                        modifier = Modifier.padding(innerPadding),
+                        modifier =
+                            Modifier
+                                .padding(innerPadding),
                         viewModel = viewModel,
                         onCloseActivity = {
                             val resultIntent =
