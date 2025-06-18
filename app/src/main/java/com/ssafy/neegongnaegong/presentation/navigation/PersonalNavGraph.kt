68da03fb236655b6cd12021c48730158f7968ae2
김민조 <upsk1@naver.com>
2025-06-18 21:50:09 +0900

    chore(#122): 실수 ㅎㅎ;

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/navigation/PersonalNavGraph.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/navigation/PersonalNavGraph.kt
@@ -5,7 +5,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import androidx.navigation.toRoute
-import com.ssafy.neegongnaegong.presentation.group.user.search.UserSearchRoute
+import com.ssafy.neegongnaegong.presentation.personal.PersonalRoute
 import com.ssafy.neegongnaegong.presentation.personal.edit.StudyRecordEditRoute
 
 fun NavGraphBuilder.personalNavGraph(navController: NavController) {
@@ -13,20 +13,17 @@ fun NavGraphBuilder.personalNavGraph(navController: NavController) {
         startDestination = AppNavigation.Screen.Personal.Main,
     ) {
         composable<AppNavigation.Screen.Personal.Main> {
-
-            UserSearchRoute { }
-
-//            PersonalRoute(
-//                popBackStack = { navController.popBackStack() },
-//                navigateToEditScreen = {
-//                    navController.navigate(
-//                        AppNavigation.Screen.Personal.Edit(
-//                            it,
-//                        ),
-//                    )
-//                },
-//                navController = navController,
-//            )
+            PersonalRoute(
+                popBackStack = { navController.popBackStack() },
+                navigateToEditScreen = {
+                    navController.navigate(
+                        AppNavigation.Screen.Personal.Edit(
+                            it,
+                        ),
+                    )
+                },
+                navController = navController,
+            )
         }
 
         composable<AppNavigation.Screen.Personal.Edit> { backStackEntry ->
