a328a4648c544374a490184cbbba9ff802cdd0bc
Na2te <111sym@naver.com>
2025-06-29 16:13:12 +0900

    fix(#140): 불필요한 Drawer 메뉴 삭제 및 Lazy Column 처리 - 그룹 생성, 그룹 검색, 일정 관리 메뉴 삭제 - Height가 작은 휴대폰의 경우 Column은 자동으로 스크롤을 채우지 않아 잘리던 문제 해결 - Lazy Column으로 영역 넘어갈 시 Scroll 되도록 함 - 메뉴 삭제로 안쓰는 인자 삭제

--- app/src/main/java/com/ssafy/neegongnaegong/presentation/group/component/drawer/StudiesDrawer.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/presentation/group/component/drawer/StudiesDrawer.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
@@ -25,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -34,6 +34,7 @@ import com.skydoves.landscapist.glide.GlideImage
 import com.ssafy.neegongnaegong.R
 import com.ssafy.neegongnaegong.presentation.group.detail.StudiesDetailContract
 import com.ssafy.neegongnaegong.presentation.group.detail.StudiesDetailViewModel
+import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongPreviews
 import com.ssafy.neegongnaegong.presentation.ui.theme.NeeGongNaeGongTheme
 
 @Composable
@@ -66,9 +67,6 @@ private fun StudiesDrawer(
     headerImageUrl: String? = null,
     name: String = "",
     description: String = "",
-    onScheduleManagementClick: () -> Unit = {},
-    onStudyCreateClick: () -> Unit = {},
-    onStudySearchClick: () -> Unit = {},
     onMyStudyClick: () -> Unit = {},
     onStudyItemClick: (Long) -> Unit = {},
     onStudyDeleteClick: () -> Unit = {},
@@ -76,22 +74,16 @@ private fun StudiesDrawer(
     navigateToStudiesMembersRole: () -> Unit = {},
     navigateToStudiesApplications: () -> Unit = {},
 ) {
-    Column(
+    LazyColumn(
         modifier =
             modifier
                 .fillMaxHeight()
                 .background(NeeGongNaeGongTheme.colorScheme.background),
     ) {
-        Box(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .height(240.dp)
-                    .background(NeeGongNaeGongTheme.colorScheme.gray3),
-        ) {
+        item {
             GlideImage(
                 imageModel = { headerImageUrl ?: R.drawable.img_main_character },
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier.fillMaxWidth().height(240.dp),
                 loading = {
                     Box(modifier = Modifier.fillMaxSize()) {
                         CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
@@ -105,141 +97,146 @@ private fun StudiesDrawer(
                 },
             )
         }
-        // 그룹 정보 부분
-        Box(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .background(color = NeeGongNaeGongTheme.colorScheme.gray2),
-        ) {
-            Column(modifier = Modifier.padding(10.dp)) {
+        item {
+            // 그룹 정보 부분
+            Box(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .background(color = NeeGongNaeGongTheme.colorScheme.gray2),
+            ) {
+                Column(modifier = Modifier.padding(10.dp)) {
+                    Text(
+                        modifier = Modifier.padding(bottom = 10.dp),
+                        text = name,
+                        style = NeeGongNaeGongTheme.typography.titleMedium,
+                        color = NeeGongNaeGongTheme.colorScheme.primaryText,
+                    )
+                    Text(
+                        text = description,
+                        style = NeeGongNaeGongTheme.typography.bodyMedium,
+                        color = NeeGongNaeGongTheme.colorScheme.primaryText,
+                    )
+                }
+            }
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+
+        item {
+            // 그룹 관리 섹션
+            DrawerMenuItem(
+                icon = R.drawable.ic_studies_drw_group_management,
+                title = stringResource(R.string.studies_drw_group_management),
+                onClick = navigateTodStudiesEdit,
+            )
+        }
+
+        item {
+            // 멤버 관리 섹션
+            DrawerMenuItem(
+                icon = R.drawable.ic_studies_drw_member_management,
+                title = stringResource(R.string.studies_drw_member_management),
+                onClick = navigateToStudiesMembersRole,
+            )
+        }
+
+        item {
+            // 스터디 삭제 버튼
+            DrawerMenuItem(
+                icon = R.drawable.ic_studies_drw_studies_delete,
+                title = stringResource(R.string.studies_drw_studies_delete),
+                onClick = onStudyDeleteClick,
+            )
+        }
+
+        item {
+            // 스터디 가입신청 현황
+            DrawerMenuItem(
+                icon = R.drawable.ic_studies_drw_studies_applications,
+                title = stringResource(R.string.studies_drw_studies_application),
+                onClick = navigateToStudiesApplications,
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+
+        item {
+            HorizontalDivider(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                thickness = 1.dp,
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+
+        item {
+            // 내 스터디 섹션
+            // TODO : 내 스터디 수 추가
+            Row(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
                 Text(
-                    modifier = Modifier.padding(bottom = 10.dp),
-                    text = name,
-                    style = NeeGongNaeGongTheme.typography.titleMedium,
+                    text = stringResource(R.string.studies_drw_my_studies),
+                    style = NeeGongNaeGongTheme.typography.titleSmall,
                     color = NeeGongNaeGongTheme.colorScheme.primaryText,
                 )
                 Text(
-                    text = description,
-                    style = NeeGongNaeGongTheme.typography.bodyMedium,
+                    text = stringResource(R.string.studies_drw_see_more),
+                    style = NeeGongNaeGongTheme.typography.bodySmall,
                     color = NeeGongNaeGongTheme.colorScheme.primaryText,
+                    modifier = Modifier.clickable { onMyStudyClick() },
                 )
             }
         }
 
-        Spacer(modifier = Modifier.height(10.dp))
-
-        // 그룹 관리 섹션
-        DrawerMenuItem(
-            icon = R.drawable.ic_studies_drw_group_management,
-            title = stringResource(R.string.studies_drw_group_management),
-            onClick = navigateTodStudiesEdit,
-        )
-
-        // 멤버 관리 섹션
-        DrawerMenuItem(
-            icon = R.drawable.ic_studies_drw_member_management,
-            title = stringResource(R.string.studies_drw_member_management),
-            onClick = navigateToStudiesMembersRole,
-        )
-
-        // 일정 관리 섹션
-        DrawerMenuItem(
-            icon = R.drawable.ic_studies_drw_schedule_management,
-            title = stringResource(R.string.studies_drw_schedule_management),
-            onClick = onScheduleManagementClick,
-        )
-
-        // 스터디 삭제 버튼
-        DrawerMenuItem(
-            icon = R.drawable.ic_studies_drw_studies_delete,
-            title = stringResource(R.string.studies_drw_studies_delete),
-            onClick = onStudyDeleteClick,
-        )
-
-        // 스터디 가입신청 현황
-        DrawerMenuItem(
-            icon = R.drawable.ic_studies_drw_studies_applications,
-            title = stringResource(R.string.studies_drw_studies_application),
-            onClick = navigateToStudiesApplications,
-        )
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        HorizontalDivider(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-            thickness = 1.dp,
-        )
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        // 내 스터디 섹션
-        // TODO : 내 스터디 수 추가
-        Row(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(
-                text = stringResource(R.string.studies_drw_my_studies),
-                style = NeeGongNaeGongTheme.typography.titleSmall,
-                color = NeeGongNaeGongTheme.colorScheme.primaryText,
-            )
-            Text(
-                text = stringResource(R.string.studies_drw_see_more),
-                style = NeeGongNaeGongTheme.typography.bodySmall,
-                color = NeeGongNaeGongTheme.colorScheme.primaryText,
-                modifier = Modifier.clickable { onMyStudyClick() },
-            )
+        item {
+            Spacer(modifier = Modifier.height(10.dp))
         }
 
-        Spacer(modifier = Modifier.height(10.dp))
-
-        // 스터디 아이콘들
-        Row(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 5.dp),
-            horizontalArrangement = Arrangement.SpaceAround,
-        ) {
-            // TODO(스터디) : 예시 스터디 아이콘들 - 실제 데이터로 교체 필요
-            CircleIcon(R.drawable.ic_app_logo_332_81) { onStudyItemClick(1) }
-            CircleIcon(R.drawable.ic_app_logo_332_81) { onStudyItemClick(2) }
-            CircleIcon(R.drawable.ic_app_logo_332_81) { onStudyItemClick(3) }
+        item {
+            // 스터디 아이콘들
+            Row(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 5.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+            ) {
+                // TODO(스터디) : 예시 스터디 아이콘들 - 실제 데이터로 교체 필요
+                CircleIcon(R.drawable.ic_app_logo_332_81) { onStudyItemClick(1) }
+                CircleIcon(R.drawable.ic_app_logo_332_81) { onStudyItemClick(2) }
+                CircleIcon(R.drawable.ic_app_logo_332_81) { onStudyItemClick(3) }
+            }
         }
 
-        Spacer(modifier = Modifier.height(10.dp))
+        item {
+            Spacer(modifier = Modifier.height(10.dp))
+        }
 
-        HorizontalDivider(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-            thickness = 1.dp,
-        )
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        // 스터디 생성 버튼
-        DrawerMenuItem(
-            icon = R.drawable.ic_studies_drw_studies_create,
-            title = stringResource(R.string.studies_drw_studies_create),
-            onClick = onStudyCreateClick,
-        )
-
-        // 스터디 검색 버튼
-        DrawerMenuItem(
-            icon = R.drawable.ic_studies_drw_studies_search,
-            title = stringResource(R.string.studies_drw_studies_search),
-            onClick = onStudySearchClick,
-        )
+        item {
+            HorizontalDivider(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                thickness = 1.dp,
+            )
+        }
     }
 }
 
@@ -269,26 +266,14 @@ fun CircleIcon(
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@NeeGongNaeGongPreviews
 @Composable
 private fun PreviewLightModeStudiesDrawer() {
     NeeGongNaeGongTheme {
         StudiesDrawer(
+            modifier = Modifier.fillMaxWidth(0.75F),
             name = "화이트 스터디",
             description = "하얗습니다.",
-        )
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-private fun PreviewDarkModeStudiesDrawer() {
-    NeeGongNaeGongTheme(
-        darkTheme = true,
-    ) {
-        StudiesDrawer(
-            name = "다크 스터디",
-            description = "어둡습니다.",
         )
     }
 }
