c55ab0e0630276fa3ecb5190e075aa7cabced026
전형선 <hsun5163@gmail.com>
2025-07-13 02:00:50 +0900

    refactor(#175): CreateStudiesUseCase flow body 제거 및 try-catch 제거
    - flatMapLatest을 활용하여 return하는 형태로 변경

--- app/src/main/java/com/ssafy/neegongnaegong/domain/usecase/studies/CreateStudiesUseCase.kt
+++ app/src/main/java/com/ssafy/neegongnaegong/domain/usecase/studies/CreateStudiesUseCase.kt
@@ -1,14 +1,15 @@
 package com.ssafy.neegongnaegong.domain.usecase.studies
 
-import android.util.Log
 import com.ssafy.neegongnaegong.domain.model.file.PresignedUrlInfo
 import com.ssafy.neegongnaegong.domain.model.file.UploadPathType
 import com.ssafy.neegongnaegong.domain.model.studies.StudyInfo
 import com.ssafy.neegongnaegong.domain.repository.FileRepository
 import com.ssafy.neegongnaegong.domain.repository.S3Repository
 import com.ssafy.neegongnaegong.domain.repository.StudiesRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
 import okhttp3.RequestBody
 import javax.inject.Inject
@@ -20,44 +21,39 @@ class CreateStudiesUseCase
         private val fileRepository: FileRepository,
         private val s3Repository: S3Repository,
     ) {
+        @OptIn(ExperimentalCoroutinesApi::class)
         operator fun invoke(
             studyInfo: StudyInfo,
             imageRequestBody: RequestBody?,
             imageExtension: String?,
-        ): Flow<Unit> =
-            flow {
-                try {
-                    // 1. 스터디 생성 API 호출 -> studyGroupId 발급
-                    val studyGroupId = studiesRepository.createStudies(studyInfo).first()
-                    // 2. studyGroupId로 Presigned URL 발급
-                    val presignedUrlInfo: PresignedUrlInfo? =
-                        imageExtension?.let {
-                            fileRepository
-                                .issuePresignedUrl(
-                                    id = studyGroupId,
-                                    uploadPathType = UploadPathType.PROFILE_STUDY.path,
-                                    imageExtension = it,
-                                ).first()
-                        }
-
-                    if (presignedUrlInfo != null && imageRequestBody != null) {
-                        // 3. 발급 받은 URL로 S3에 이미지 업로드
-                        s3Repository.uploadImageToS3(
-                            presignedUrl = presignedUrlInfo.presignedUrl,
-                            request = imageRequestBody,
-                        )
-                        // 4. 등록한 이미지 변경
-                        studiesRepository
-                            .changeStudiesProfileImage(
-                                studyGroupId = studyGroupId,
-                                profileImage = presignedUrlInfo.imageUrl,
+        ): Flow<Unit> {
+            // 1. 스터디 생성 API 호출 -> studyGroupId 발급
+            return studiesRepository.createStudies(studyInfo).flatMapLatest { studyGroupId ->
+                // 2. studyGroupId로 Presigned URL 발급
+                val presignedUrlInfo: PresignedUrlInfo? =
+                    imageExtension?.let {
+                        fileRepository
+                            .issuePresignedUrl(
+                                id = studyGroupId,
+                                uploadPathType = UploadPathType.PROFILE_STUDY.path,
+                                imageExtension = it,
                             ).first()
-                        // 구독 필요
                     }
-                    emit(Unit)
-                } catch (e: Exception) {
-                    Log.e("CreateStudiesUseCase", "${e.message}")
-                    throw e
+                if (presignedUrlInfo != null && imageRequestBody != null) {
+                    // 3. 발급 받은 URL로 S3에 이미지 업로드
+                    s3Repository.uploadImageToS3(
+                        presignedUrl = presignedUrlInfo.presignedUrl,
+                        request = imageRequestBody,
+                    )
+                    // 4. 등록한 이미지 변경
+                    studiesRepository
+                        .changeStudiesProfileImage(
+                            studyGroupId = studyGroupId,
+                            profileImage = presignedUrlInfo.imageUrl,
+                        )
+                } else {
+                    flow { emit(Unit) }
                 }
             }
+        }
     }
